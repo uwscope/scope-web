377a261806e68c17e752584dddcf84de6208ffa6
Anant Mittal <a42@Anants-MacBook-Pro.local>
2021-10-28 17:06:09 -0700

    Set the dynamic local port as an flask environment variable for dev environment

--- tasks/flask.py
+++ tasks/flask.py
@@ -2,12 +2,12 @@
 Tasks for the Flask server.
 """
 
-from aws_infrastructure.tasks.collection import compose_collection
+from pathlib import Path
+
 import aws_infrastructure.tasks.library.documentdb
 import aws_infrastructure.tasks.ssh
-from invoke import Collection
-from invoke import task
-from pathlib import Path
+from aws_infrastructure.tasks.collection import compose_collection
+from invoke import Collection, task
 
 from tasks.terminal import spawn_new_terminal
 
@@ -25,15 +25,18 @@ def dev_serve(context):
     """
 
     if spawn_new_terminal(context):
-        ssh_config = aws_infrastructure.tasks.ssh.SSHConfig.load(SSH_CONFIG_PATH)
-        documentdb_config = aws_infrastructure.tasks.library.documentdb.DocumentDBConfig.load(DOCUMENTDB_CONFIG_PATH)
+
+        ssh_config = aws_infrastructure.tasks.ssh.SSHConfig.load(
+            SSH_CONFIG_PATH)
+        documentdb_config = aws_infrastructure.tasks.library.documentdb.DocumentDBConfig.load(
+            DOCUMENTDB_CONFIG_PATH)
 
         with aws_infrastructure.tasks.ssh.SSHClientContextManager(ssh_config=ssh_config) as ssh_client:
             with aws_infrastructure.tasks.ssh.SSHPortForwardContextManager(
                 ssh_client=ssh_client,
                 remote_host=documentdb_config.endpoint,
                 remote_port=documentdb_config.port,
-            ):
+            ) as ssh_port_forward:
                 with context.cd(Path(FLASK_DIR)):
                     context.run(
                         command=' '.join([
@@ -46,6 +49,7 @@ def dev_serve(context):
                             'FLASK_ENV': 'development',
                             'FLASK_RUN_HOST': 'localhost',
                             'FLASK_RUN_PORT': '4000',
+                            'LOCAL_DOCUMENTDB_PORT': str(ssh_port_forward.local_port)
                         },
                     )
 
