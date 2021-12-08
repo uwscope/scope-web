7733479419321d37507c7780f701cb21148420d7
James Fogarty <jayfo@users.noreply.github.com>
2021-12-08 08:00:15 -0800

    Code Format

--- tasks/flask.py
+++ tasks/flask.py
@@ -11,9 +11,9 @@ from pathlib import Path
 
 from tasks.terminal import spawn_new_terminal
 
-FLASK_DIR = './server_flask'
-SSH_CONFIG_PATH = './secrets/configuration/ssh.yaml'
-DOCUMENTDB_CONFIG_PATH = './secrets/configuration/documentdb.yaml'
+FLASK_DIR = "./server_flask"
+SSH_CONFIG_PATH = "./secrets/configuration/ssh.yaml"
+DOCUMENTDB_CONFIG_PATH = "./secrets/configuration/documentdb.yaml"
 
 
 @task
@@ -26,9 +26,15 @@ def dev_serve(context):
 
     if spawn_new_terminal(context):
         ssh_config = aws_infrastructure.tasks.ssh.SSHConfig.load(SSH_CONFIG_PATH)
-        documentdb_config = aws_infrastructure.tasks.library.documentdb.DocumentDBConfig.load(DOCUMENTDB_CONFIG_PATH)
+        documentdb_config = (
+            aws_infrastructure.tasks.library.documentdb.DocumentDBConfig.load(
+                DOCUMENTDB_CONFIG_PATH
+            )
+        )
 
-        with aws_infrastructure.tasks.ssh.SSHClientContextManager(ssh_config=ssh_config) as ssh_client:
+        with aws_infrastructure.tasks.ssh.SSHClientContextManager(
+            ssh_config=ssh_config
+        ) as ssh_client:
             with aws_infrastructure.tasks.ssh.SSHPortForwardContextManager(
                 ssh_client=ssh_client,
                 remote_host=documentdb_config.endpoint,
@@ -38,16 +44,18 @@ def dev_serve(context):
                     context.run(
                         # Instead of using `flask run`, import the app normally, then run it.
                         # Did this because `flask run` was eating an ImportError, not giving a useful error message.
-                        command=' '.join([
-                            'pipenv',
-                            'run',
-                            'python',
-                            'app.py',
-                        ]),
+                        command=" ".join(
+                            [
+                                "pipenv",
+                                "run",
+                                "python",
+                                "app.py",
+                            ]
+                        ),
                         env={
-                            'FLASK_ENV': 'development',
-                            'FLASK_RUN_HOST': 'localhost',
-                            'FLASK_RUN_PORT': '4000',
+                            "FLASK_ENV": "development",
+                            "FLASK_RUN_HOST": "localhost",
+                            "FLASK_RUN_PORT": "4000",
                         },
                     )
 
@@ -62,27 +70,29 @@ def prod_serve(context):
 
     with context.cd(Path(FLASK_DIR)):
         context.run(
-            command=' '.join([
-                'pipenv',
-                'run',
-                'waitress-serve',
-                '--port=4000',
-                '--call "app:create_app"'
-            ]),
+            command=" ".join(
+                [
+                    "pipenv",
+                    "run",
+                    "waitress-serve",
+                    "--port=4000",
+                    '--call "app:create_app"',
+                ]
+            ),
             env={
-                'FLASK_ENV': 'production'
-            }
+                "FLASK_ENV": "production",
+            },
         )
 
 
 # Build task collection
-ns = Collection('flask')
+ns = Collection("flask")
 
-ns_dev = Collection('dev')
-ns_dev.add_task(dev_serve, 'serve')
+ns_dev = Collection("dev")
+ns_dev.add_task(dev_serve, "serve")
 
-ns_prod = Collection('prod')
-ns_prod.add_task(prod_serve, 'serve')
+ns_prod = Collection("prod")
+ns_prod.add_task(prod_serve, "serve")
 
-compose_collection(ns, ns_dev, name='dev')
-compose_collection(ns, ns_prod, name='prod')
+compose_collection(ns, ns_dev, name="dev")
+compose_collection(ns, ns_prod, name="prod")
