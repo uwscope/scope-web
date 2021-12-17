a0461fa1cdcfb75aa5a46634c494703383e35fba
James Fogarty <jayfo@users.noreply.github.com>
2021-12-17 12:00:16 -0800

    Refactor Old Reachability Test to Use Config Fixtures

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,85 +1,26 @@
 import aws_infrastructure.tasks.ssh
 from pymongo import MongoClient
-import pytest
 
-import scope.config
-import scope.testing.testing_config
+import tests.testing_config
 
-
-SSH_CONFIGS = {
-    "host": aws_infrastructure.tasks.ssh.SSHConfig.load(
-        ssh_config_path="./secrets/configuration/instance_ssh.yaml",
-    ),
-}
-
-DOCUMENTDB_CONFIGS = {
-    "experimental": scope.config.DocumentDBConfig.load(
-        config_path="./secrets/configuration/documentdb.yaml",
-    ),
-}
-
-
-@pytest.fixture(
-    name="config_ssh_client",
-    # TODO: this won't allow modules to choose configurations
-    params=SSH_CONFIGS.values(),
-    ids=SSH_CONFIGS.keys(),
-)
-def fixture_config_ssh_client(
-    request: pytest.FixtureRequest,
-) -> aws_infrastructure.tasks.ssh.SSHConfig:
-    """
-    Obtain SSH client configuration.
-    """
-    return request.param
-
-
-@pytest.fixture(
-    name="config_documentdb",
-    # TODO: this won't allow modules to choose configurations
-    params=DOCUMENTDB_CONFIGS.values(),
-    ids=DOCUMENTDB_CONFIGS.keys(),
-)
-def fixture_config_documentdb(
-    request: pytest.FixtureRequest,
-) -> scope.config.DocumentDBConfig:
-    """
-    Obtain DocumentDB configuration.
-    """
-    return request.param
-
-
-@pytest.fixture(
-    name="config_documentdb_client",
-)
-def fixture_config_documentdb_client(
-    config_documentdb: scope.config.DocumentDBConfig,
-) -> scope.config.DocumentDBClientConfig:
-    """
-    Obtain DocumentDB client configuration.
-    """
-    return scope.config.DocumentDBClientConfig(
-        endpoint=config_documentdb.endpoint,
-        hosts=config_documentdb.hosts,
-        port=config_documentdb.port,
-    )
+TESTING_CONFIGS = tests.testing_config.ALL_CONFIGS
 
 
 def test_documentdb_reachable(
-    config_ssh_client,
-    config_documentdb,
+    instance_ssh_config,
+    documentdb_config,
 ):
     """
     Test that we can reach the DocumentDB cluster hosting the database.
     """
 
     with aws_infrastructure.tasks.ssh.SSHClientContextManager(
-        ssh_config=config_ssh_client
+        ssh_config=instance_ssh_config
     ) as ssh_client:
         with aws_infrastructure.tasks.ssh.SSHPortForwardContextManager(
             ssh_client=ssh_client,
-            remote_host=config_documentdb.endpoint,
-            remote_port=config_documentdb.port,
+            remote_host=documentdb_config.endpoint,
+            remote_port=documentdb_config.port,
         ) as ssh_port_forward:
             # Obtain a client authenticated as DocumentDB admin
             client_admin = MongoClient(
@@ -91,8 +32,8 @@ def test_documentdb_reachable(
                 # Because of the port forward, must not attempt to access the replica set
                 directConnection=True,
                 # Connect as admin
-                username=config_documentdb.admin_user,
-                password=config_documentdb.admin_password,
+                username=documentdb_config.admin_user,
+                password=documentdb_config.admin_password,
                 # DocumentDB requires SSL, but port forwarding means the certificate will not match
                 tls=True,
                 tlsInsecure=True,
