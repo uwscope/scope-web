6eec8ced0fc24b488c772a765dd277bd8877d6c2
James Fogarty <jayfo@users.noreply.github.com>
2021-12-20 07:46:11 -0800

    Refactor Test Modules

--- scope_shared/scope/testing/test_check_fixtures.py
+++
@@ -1,9 +0,0 @@
-"""
-Import all tests that should be executed to check all fixtures.
-
-Packages can then import this single module for their own check fixtures.
-"""
-
-from scope.testing.test_check_fixtures_database import *
-from scope.testing.test_check_fixtures_documentdb import *
-from scope.testing.test_check_fixtures_flask import *
