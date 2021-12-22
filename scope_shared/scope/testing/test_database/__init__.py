d5fbed0fba62cec71328e62769e48a3e567b5647
James Fogarty <jayfo@users.noreply.github.com>
2021-12-21 17:07:22 -0800

    Split Out a Database Model

---
+++ scope_shared/scope/testing/test_database/__init__.py
@@ -0,0 +1,7 @@
+"""
+Import all database tests.
+
+Packages can then import this single module for their own database tests.
+"""
+
+from scope.testing.test_database.test_patients import *
--- server_flask/database.py
+++ server_flask/database.py
@@ -16,14 +16,25 @@ class Database:
     @staticmethod
     def init_app(*, app: flask.Flask,):
         client = pymongo.MongoClient(
+            # Synchronously initiate the connection
             connect=True,
+            # Connect to the provided host
             host=app.config["DOCUMENTDB_HOST"],
             port=app.config["DOCUMENTDB_PORT"],
+            # Based on configuration, direct connect or use replica set
             directConnection=app.config["DOCUMENTDB_DIRECTCONNECTION"],
+            # DocumentDB requires SSL, configure whether certificates are expected to be valid
             tls=True,
             tlsInsecure=app.config["DOCUMENTDB_TLSINSECURE"],
+            # PyMongo defaults to retryable writes, which are not supported by DocumentDB
+            # https://docs.aws.amazon.com/documentdb/latest/developerguide/functional-differences.html#functional-differences.retryable-writes
+            retryWrites=False,
+            # Configure user and password
             username=app.config["DATABASE_USER"],
             password=app.config["DATABASE_PASSWORD"],
         )
 
-        app.database = client.get_database(app.config["DATABASE_NAME"])
+        database = client.get_database(app.config["DATABASE_NAME"])
+
+        # Store the database client on the Flask app
+        app.database = database
