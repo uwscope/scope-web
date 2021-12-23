a517f575b7e4d5b4e22b55892388ccdb47fbe150
Anant Mittal <anmittal@cs.washington.edu>
2021-12-23 14:42:23 -0800

    updated URI

--- scope_shared/scope/schema.py
+++ scope_shared/scope/schema.py
@@ -8,7 +8,7 @@ catalog = create_catalog("2020-12")
 
 # NOTE: For the subschemas to work, the schemas must be loaded in either "$ref" dependency order, or the base URI-to-directory mapping on the catalog needs to be set up (`.add_directory`) as shown below.
 
-catalog.add_directory(URI("https://example.com/schemas/"), schema_dir)
+catalog.add_directory(URI("https://uwscope.org/schemas/"), schema_dir)
 
 
 patient_schema = JSONSchema.loadf(schema_dir / "patient.json")
