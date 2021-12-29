5d3037150d027029eb0d147521afbaff4f5e7f64
Anant Mittal <anmittal@cs.washington.edu>
2021-12-29 09:30:43 -0800

    added logging code for debugging

--- server_flask/utils.py
+++ server_flask/utils.py
@@ -1,8 +1,9 @@
 import json
 from functools import wraps
 
-from flask import abort, jsonify, request
+from flask import abort, current_app, jsonify, request
 from flask_json import json_response
+from jschon import JSON
 
 
 def parseInt(text):
@@ -10,14 +11,17 @@ def parseInt(text):
 
 
 # TODO: Confirm location of the function with James.
+
+
 def validate_schema(schema_object):
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
             # For .evaluate to work, its argument needs to be of type '<class 'jschon.json.JSON'>'
-            result = schema_object.evaluate(
-                json_response.loads(json.dumps(request.json))
-            )
+            current_app.logger.info(schema_object)
+            result = schema_object.evaluate(JSON.loads(json.dumps(request.json)))
+            current_app.logger.info(result.output("basic"))
+
             if result.output("flag")["valid"] == False:
                 abort(
                     400,
