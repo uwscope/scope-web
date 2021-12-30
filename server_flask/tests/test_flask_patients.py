ba97b128cf94113bd8ba4ff9cee3f6b9da0a8dff
Anant Mittal <anmittal@cs.washington.edu>
2021-12-30 09:13:07 -0800

    added get all patients test

--- server_flask/tests/test_flask_patients.py
+++ server_flask/tests/test_flask_patients.py
@@ -11,6 +11,46 @@ import tests.testing_config
 TESTING_CONFIGS = tests.testing_config.ALL_CONFIGS
 
 
+def test_flask_get_all_patients_actual(
+    database_client: pymongo.database.Database,
+    flask_client_config: scope.config.FlaskClientConfig,
+    flask_session_unauthenticated_factory: Callable[[], requests.Session],
+    data_fake_patient_factory: Callable[[], dict],
+):
+    """
+    Test that we can get a list of patients.
+    """
+
+    # Generate a fake patient
+    data_fake_patient = data_fake_patient_factory()
+
+    # Insert the fake patient
+    scope.database.patients.create_patient_collection(
+        database=database_client,
+        patient=data_fake_patient,
+    )
+
+    # Obtain a session
+    session = flask_session_unauthenticated_factory()
+
+    # Retrieve all patients
+    response = session.get(
+        url=urljoin(
+            flask_client_config.baseurl,
+            "patients/",
+        ),
+    )
+    assert response.ok
+    # print(response.json())
+    # print(data_fake_patient)
+
+    # "patients" is a list
+    response_patients = response.json()["patients"]
+
+    # Ensure list includes our fake patient
+    assert data_fake_patient in response_patients
+
+
 def test_flask_get_all_patients(
     database_client: pymongo.database.Database,
     flask_client_config: scope.config.FlaskClientConfig,
