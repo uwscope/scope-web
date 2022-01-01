40112d3a6cc20b160e9700c1b3541a3bbe4ea08e
James Fogarty <jayfo@users.noreply.github.com>
2022-01-01 14:19:32 -0800

    Simplify Testing Config

--- tests/testing_config.py
+++ tests/testing_config.py
@@ -3,17 +3,21 @@ import aws_infrastructure.tasks.ssh
 import scope.config
 import scope.testing.testing_config
 
+DOCUMENTDB_CONFIG = scope.config.DocumentDBConfig.load(
+    config_path="./secrets/configuration/documentdb.yaml",
+)
+
+DEV_DATABASE_CONFIG = scope.config.DatabaseConfig.load(
+    config_path="./secrets/configuration/dev_database.yaml",
+)
+
 DEVELOPMENT_CONFIGS = {
     "development_local": scope.testing.testing_config.TestingConfig(
         instance_ssh_config=aws_infrastructure.tasks.ssh.SSHConfig.load(
             ssh_config_path="./secrets/configuration/instance_ssh.yaml",
         ),
-        documentdb_config=scope.config.DocumentDBConfig.load(
-            config_path="./secrets/configuration/documentdb.yaml",
-        ),
-        database_config=scope.config.DatabaseConfig.load(
-            config_path="./secrets/configuration/dev_database.yaml",
-        ),
+        documentdb_config=DOCUMENTDB_CONFIG,
+        database_config=DEV_DATABASE_CONFIG,
         flask_config=scope.config.FlaskConfig.load(
             config_path="./secrets/configuration/dev_local_flask.yaml",
         ),
@@ -23,9 +27,9 @@ DEVELOPMENT_CONFIGS = {
 ALL_CONFIGS = DEVELOPMENT_CONFIGS
 
 UNIQUE_DOCUMENTDB_CONFIGS = [
-    DEVELOPMENT_CONFIGS["development_local"].documentdb_config,
+    DOCUMENTDB_CONFIG,
 ]
 
 UNIQUE_DATABASE_CONFIGS = [
-    DEVELOPMENT_CONFIGS["development_local"].database_config,
+    DEV_DATABASE_CONFIG,
 ]
