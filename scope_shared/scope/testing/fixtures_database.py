bb40b65647e747b215b39d82d982699c0983d371
James Fogarty <jayfo@users.noreply.github.com>
2022-01-05 07:11:14 -0800

    Database User Initialization

--- scope_shared/scope/testing/fixtures_database.py
+++ scope_shared/scope/testing/fixtures_database.py
@@ -1,3 +1,4 @@
+import aws_infrastructure.tasks.ssh
 import pymongo
 import pymongo.database
 import pytest
@@ -7,9 +8,11 @@ import scope.testing
 
 
 def _fixture_database_client(
+    *,
     explicit_check_fixtures: bool,
+    documentdb_config: scope.config.DocumentDBConfig,
+    documentdb_port_forward: aws_infrastructure.tasks.ssh.SSHPortForward,
     database_config: scope.config.DatabaseConfig,
-    documentdb_client: pymongo.MongoClient,
 ) -> pymongo.database.Database:
     """
     Worker for fixture_database_client. Separated to allow fixture check for session scope.
@@ -22,15 +25,37 @@ def _fixture_database_client(
         #
         # Create the client
         #
-
-        database_client = documentdb_client.get_database(database_config.name)
+        # TODO Refactor all MongoClient creation into scope.database
+        documentdb_client = pymongo.MongoClient(
+            # Synchronously initiate the connection
+            connect=True,
+            # Connect via SSH port forward
+            host="127.0.0.1",
+            port=documentdb_port_forward.local_port,
+            # Because of the port forward, must not attempt to access the replica set
+            directConnection=True,
+            # DocumentDB requires SSL, but port forwarding means the certificate will not match
+            tls=True,
+            tlsInsecure=True,
+            # PyMongo defaults to retryable writes, which are not supported by DocumentDB
+            # https://docs.aws.amazon.com/documentdb/latest/developerguide/functional-differences.html#functional-differences.retryable-writes
+            retryWrites=False,
+            # Connect as database user
+            username=database_config.user,
+            password=database_config.password,
+        )
 
         #
         # Probe the client
         #
 
         # Ensure the database exists
-        assert database_client.name in documentdb_client.list_database_names()
+        assert database_config.name in documentdb_client.list_database_names()
+
+        # Obtain list of collections in the database, this requires authentication
+        database_client = documentdb_client.get_database(database_config.name)
+        response = database_client.list_collection_names()
+        assert isinstance(response, list)
 
         return database_client
     except Exception:
@@ -48,8 +73,9 @@ def _fixture_database_client(
 
 @pytest.fixture(name="database_client", scope="session")
 def fixture_database_client(
+    documentdb_config: scope.config.DocumentDBConfig,
+    documentdb_port_forward: aws_infrastructure.tasks.ssh.SSHPortForward,
     database_config: scope.config.DatabaseConfig,
-    documentdb_client_admin: pymongo.MongoClient,
 ) -> pymongo.database.Database:
     """
     Fixture for obtaining a client for the database.
@@ -61,6 +87,7 @@ def fixture_database_client(
 
     return _fixture_database_client(
         explicit_check_fixtures=False,
+        documentdb_config=documentdb_config,
+        documentdb_port_forward=documentdb_port_forward,
         database_config=database_config,
-        documentdb_client=documentdb_client_admin,
     )
