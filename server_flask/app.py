a1080b71f1f74da802f5b9419ac317939f6cefba
Anant Mittal <anmittal@cs.washington.edu>
2022-01-06 09:49:50 -0800

    Refactored code to improve readability

--- server_flask/app.py
+++ server_flask/app.py
@@ -8,9 +8,9 @@ from markupsafe import escape
 
 import database
 from assessments import get_supported_assessments
-from blueprints.patient.values import patient_values_blueprint
+from blueprints.patient.values_inventory import patient_values_blueprint
 from blueprints.registry.patients import registry_patients_blueprint
-from blueprints.registry.values import registry_values_blueprint
+from blueprints.registry.values_inventory import registry_values_blueprint
 from fake import getFakePatient, getRandomFakePatients
 from utils import parseInt
 
