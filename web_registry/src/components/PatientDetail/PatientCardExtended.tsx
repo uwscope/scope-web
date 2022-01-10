3b647e770fd075e2135e7310f1c95b0aaddef05e
Jina Suh <jasmart@gmail.com>
2022-01-10 14:29:07 -0800

    Colored flags in patient card

--- web_registry/src/components/PatientDetail/PatientCardExtended.tsx
+++ web_registry/src/components/PatientDetail/PatientCardExtended.tsx
@@ -55,14 +55,16 @@ export const PatientCardExtended: FunctionComponent = observer((_) => {
 
             <LabeledField label="Flags" value="" />
             <FlaggedField
-                label="Discussion"
-                flagged={flaggedForDiscussion}
-                onClick={() => onToggleFlag('Flag for discussion', !flaggedForDiscussion)}
-            />
-            <FlaggedField
                 label="Safety risk"
                 flagged={flaggedForSafety}
+                color={'bad'}
                 onClick={() => onToggleFlag('Flag as safety risk', !flaggedForSafety)}
+            />
+            <FlaggedField
+                label="Discussion"
+                flagged={flaggedForDiscussion}
+                color={'warning'}
+                onClick={() => onToggleFlag('Flag for discussion', !flaggedForDiscussion)}
             />
         </Container>
     );
