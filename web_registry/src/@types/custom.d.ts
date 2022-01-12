67559a580dc1ff167fb4a3f89a8c75aa94b0bf2d
James Fogarty <jayfo@users.noreply.github.com>
2022-01-12 08:57:06 -0800

    Enable Runtime Loading of CLIENT_CONFIG

--- web_registry/src/@types/custom.d.ts
+++ web_registry/src/@types/custom.d.ts
@@ -7,5 +7,7 @@ declare module '*.png' {
 declare function setTimeout(callback: () => void, ms: number): number;
 declare function clearTimeout(timeoutId: number): void;
 
-// Environment variables
-declare var __API__: string;
+// Expected client configuration
+declare var CLIENT_CONFIG: {
+    flaskBaseUrl: string
+}
