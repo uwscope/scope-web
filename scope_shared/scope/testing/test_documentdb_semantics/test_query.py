2cc1001ac1862546f4f2867fa897dbb6ca67f8ef
James Fogarty <jayfo@users.noreply.github.com>
2022-01-21 20:29:33 -0800

    Test of Query Semantics

--- scope_shared/scope/testing/test_documentdb_semantics/test_query.py
+++ scope_shared/scope/testing/test_documentdb_semantics/test_query.py
@@ -1,92 +1,157 @@
-# import pprint
-# from typing import Callable
-#
-# import pymongo.database
-# import pymongo.errors
-# import pytest
-# import scope.database.patients
-#
-# PP = pprint.PrettyPrinter(indent=4)
-#
-#
-# # @pytest.mark.skip(reason="no way of currently testing this")
-# def test_sessions_pymongo_tutorial_one(
-#     database_client: pymongo.database.Database,
-#     data_fake_patient_factory: Callable[[], dict],
-# ):
-#     # Generate a fake patient
-#     data_fake_patient = data_fake_patient_factory()
-#
-#     patient_collection_name = scope.database.patients.create_patient(
-#         database=database_client, patient=data_fake_patient
-#     )
-#
-#     session_documents = data_fake_patient.get("sessions")
-#
-#     session_revised_document = session_documents[0]
-#     session_revised_document.pop("_id")
-#     session_revised_document["_rev"] += 1
-#
-#     database_client[patient_collection_name].insert_one(session_revised_document)
-#
-#     pipeline = [
-#         {"$match": {"_type": "session"}},
-#         {
-#             "$group": {
-#                 "_id": "$_session_id",
-#                 # Count the number of revison sessions in the group:
-#                 "session_count": {"$sum": 1},
-#                 # Create a list of _rev value
-#                 "session_revs": {"$push": "$_rev"},
-#             }
-#         },
-#         {"$sort": {"_rev": pymongo.DESCENDING}},
-#     ]
-#
-#     PP.pprint(list(database_client[patient_collection_name].aggregate(pipeline)))
-#
-#     database_client.drop_collection(patient_collection_name)
-#
-#
-# def test_sessions_pymongo_tutorial_two(
-#     database_client: pymongo.database.Database,
-#     data_fake_patient_factory: Callable[[], dict],
-# ):
-#     # Generate a fake patient
-#     data_fake_patient = data_fake_patient_factory()
-#
-#     patient_collection_name = scope.database.patients.create_patient(
-#         database=database_client, patient=data_fake_patient
-#     )
-#
-#     session_documents = data_fake_patient.get("sessions")
-#
-#     # Revise all documents in session_documents
-#     for sd in session_documents:
-#         sd.pop("_id", None)
-#         sd["_rev"] += 1
-#
-#     database_client[patient_collection_name].insert_many(session_documents)
-#
-#     pipeline = [
-#         {"$match": {"_type": "session"}},
-#         {"$sort": {"_rev": pymongo.DESCENDING}},
-#         {
-#             "$group": {
-#                 "_id": "$_session_id",
-#                 "latest_session_document": {"$first": "$$ROOT"},
-#             }
-#         },
-#         {"$replaceRoot": {"newRoot": "$latest_session_document"}},
-#     ]
-#
-#     # PP.pprint(
-#     #     list(database_client[patient_collection_name].aggregate(pipeline_option_one))
-#     # )
-#
-#     found_sessions = list(database_client[patient_collection_name].aggregate(pipeline))
-#
-#     for fs in found_sessions:
-#         assert fs["_rev"] == 2
-#
-#     # database_client.drop_collection(patient_collection_name)
+import pymongo.database
+import pymongo.errors
+import pytest
+from typing import Callable
+
+
+def test_singleton_query(
+    temp_collection_client_factory: Callable[[], pymongo.collection.Collection],
+):
+    """
+    Query a singleton.
+    """
+
+    collection = temp_collection_client_factory()
+
+    # Index structure based on test_index.py
+    collection.create_index(
+        [
+            ("_type", pymongo.ASCENDING),
+            ("_set_id", pymongo.ASCENDING),
+            ("_rev", pymongo.DESCENDING),
+        ],
+        unique=True,
+        name="index",
+    )
+
+    # Populate some documents
+    result = collection.insert_many(
+        [
+            {"_type": "singleton", "_rev": "1"},
+            {"_type": "singleton", "_rev": "2"},
+            {"_type": "other singleton", "_rev": "1"},
+            {"_type": "other singleton", "_rev": "2"},
+            {"_type": "set", "_set_id": "1", "_rev": "1"},
+            {"_type": "set", "_set_id": "1", "_rev": "2"},
+            {"_type": "set", "_set_id": "2", "_rev": "1"},
+            {"_type": "set", "_set_id": "2", "_rev": "2"},
+            {"_type": "other set", "_set_id": "1", "_rev": "1"},
+            {"_type": "other set", "_set_id": "1", "_rev": "2"},
+        ]
+    )
+    assert len(result.inserted_ids) == 10
+
+    # Query pipeline
+    query_document_type = "singleton"
+    pipeline = [
+        {"$match": {"_type": query_document_type}},
+        {"$sort": {"_rev": pymongo.DESCENDING}},
+        {
+            "$group": {
+                "_id": None,
+                "result": {"$first": "$$ROOT"},
+            }
+        },
+        {"$replaceRoot": {"newRoot": "$result"}},
+    ]
+
+    # Execute pipeline, obtain single result
+    with collection.aggregate(pipeline) as pipeline_result:
+        result = pipeline_result.next()
+
+    # Remove the "_id" field that was created upon insertion
+    del result["_id"]
+
+    # Confirm expected result
+    assert result == {"_type": "singleton", "_rev": "2"}
+
+
+def test_set_query(
+    temp_collection_client_factory: Callable[[], pymongo.collection.Collection],
+):
+    """
+    Query an element from a set.
+    """
+
+    collection = temp_collection_client_factory()
+
+    # Index structure based on test_index.py
+    collection.create_index(
+        [
+            ("_type", pymongo.ASCENDING),
+            ("_set_id", pymongo.ASCENDING),
+            ("_rev", pymongo.DESCENDING),
+        ],
+        unique=True,
+        name="index",
+    )
+
+    # Populate some documents
+    result = collection.insert_many(
+        [
+            {"_type": "singleton", "_rev": "1"},
+            {"_type": "singleton", "_rev": "2"},
+            {"_type": "other singleton", "_rev": "1"},
+            {"_type": "other singleton", "_rev": "2"},
+            {"_type": "set", "_set_id": "1", "_rev": "1"},
+            {"_type": "set", "_set_id": "1", "_rev": "2"},
+            {"_type": "set", "_set_id": "2", "_rev": "1"},
+            {"_type": "set", "_set_id": "2", "_rev": "2"},
+            {"_type": "other set", "_set_id": "1", "_rev": "1"},
+            {"_type": "other set", "_set_id": "1", "_rev": "2"},
+        ]
+    )
+    assert len(result.inserted_ids) == 10
+
+    # Query pipeline for all elements of a set
+    query_document_type = "set"
+    pipeline = [
+        {"$match": {"_type": query_document_type}},
+        {"$sort": {"_rev": pymongo.DESCENDING}},
+        {
+            "$group": {
+                "_id": "$_set_id",
+                "result": {"$first": "$$ROOT"},
+            }
+        },
+        {"$replaceRoot": {"newRoot": "$result"}},
+    ]
+
+    # Execute pipeline, obtain list of results
+    with collection.aggregate(pipeline) as pipeline_result:
+        result = list(pipeline_result)
+
+    # Remove the "_id" field that was created upon insertion
+    for result_current in result:
+        del result_current["_id"]
+
+    # Confirm expected result
+    assert result == [
+        {"_type": "set", "_set_id": "1", "_rev": "2"},
+        {"_type": "set", "_set_id": "2", "_rev": "2"},
+    ]
+
+    # Query pipeline for single element of a set
+    query_document_type = "set"
+    query_set_id = "1"
+    pipeline = [
+        {"$match": {"_type": query_document_type, "_set_id": query_set_id}},
+        {"$sort": {"_rev": pymongo.DESCENDING}},
+        {
+            "$group": {
+                "_id": "$_set_id",
+                "result": {"$first": "$$ROOT"},
+            }
+        },
+        {"$replaceRoot": {"newRoot": "$result"}},
+    ]
+
+    # Execute pipeline, obtain single result
+    with collection.aggregate(pipeline) as pipeline_result:
+        result = pipeline_result.next()
+
+    # Remove the "_id" field that was created upon insertion
+    del result["_id"]
+
+    # Confirm expected result
+    assert result == {"_type": "set", "_set_id": "1", "_rev": "2"}
