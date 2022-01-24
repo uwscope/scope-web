38ac0a051e31929b9e682a9f6954b82e9a5631e5
James Fogarty <jayfo@users.noreply.github.com>
2022-01-24 12:07:16 -0800

    Scaffold Database Populate Task

--- scope_shared/scope/tasks/database.py
+++ scope_shared/scope/tasks/database.py
@@ -1,10 +1,91 @@
 import aws_infrastructure.tasks.ssh
+import contextlib
 from invoke import task
 from pathlib import Path
 from typing import Union
 
 import scope.config
 import scope.database.initialize
+import scope.documentdb.client
+import scope.testing.fake_data.populate
+
+
+def _initialize(
+    *,
+    instance_ssh_config: aws_infrastructure.tasks.ssh.SSHConfig,
+    documentdb_config: scope.config.DocumentDBConfig,
+    database_config: scope.config.DatabaseConfig,
+):
+    """
+    Initialize a database, bringing it from non-existent to our expected state (e.g., it will pass all tests).
+
+    Initialization should be idempotent.
+    """
+
+    with contextlib.ExitStack() as context_manager:
+        # Connect as cluster admin for database initialization
+        documentdb_client_admin = scope.documentdb.client.documentdb_client_admin(
+            context_manager=context_manager,
+            instance_ssh_config=instance_ssh_config,
+            host=documentdb_config.endpoint,
+            port=documentdb_config.port,
+            direct_connection=True,
+            tls_insecure=True,
+            admin_user=documentdb_config.admin_user,
+            admin_password=documentdb_config.admin_password,
+        )
+
+        scope.database.initialize.ensure_database_exists(
+            documentdb_client_admin=documentdb_client_admin,
+            database_config=database_config,
+        )
+
+    with contextlib.ExitStack() as context_manager:
+        # Connect as database user for continued initialization
+        database = scope.documentdb.client.documentdb_client_database(
+            context_manager=context_manager,
+            instance_ssh_config=instance_ssh_config,
+            host=documentdb_config.endpoint,
+            port=documentdb_config.port,
+            direct_connection=True,
+            tls_insecure=True,
+            database_name=database_config.name,
+            user=database_config.user,
+            password=database_config.password,
+        )
+
+        scope.database.initialize.ensure_database_initialized(database=database)
+
+
+def _reset(
+    *,
+    instance_ssh_config: aws_infrastructure.tasks.ssh.SSHConfig,
+    documentdb_config: scope.config.DocumentDBConfig,
+    database_config: scope.config.DatabaseConfig,
+):
+    """
+    Reset a database, by dropping it then initializing it.
+    """
+
+    with contextlib.ExitStack() as context_manager:
+        documentdb_client_admin = scope.documentdb.client.documentdb_client_admin(
+            context_manager=context_manager,
+            instance_ssh_config=instance_ssh_config,
+            host=documentdb_config.endpoint,
+            port=documentdb_config.port,
+            direct_connection=True,
+            tls_insecure=True,
+            admin_user=documentdb_config.admin_user,
+            admin_password=documentdb_config.admin_password,
+        )
+
+        documentdb_client_admin.drop_database(name_or_database=database_config.name)
+
+    _initialize(
+        instance_ssh_config=instance_ssh_config,
+        documentdb_config=documentdb_config,
+        database_config=database_config,
+    )
 
 
 def task_initialize(
@@ -23,7 +104,7 @@ def task_initialize(
         Initialize the {} database.
         """
 
-        scope.database.initialize.initialize(
+        _initialize(
             instance_ssh_config=instance_ssh_config,
             documentdb_config=documentdb_config,
             database_config=database_config,
@@ -32,6 +113,45 @@ def task_initialize(
     initialize.__doc__ = initialize.__doc__.format(database_config.name)
 
     return initialize
+
+
+def task_populate(
+    *,
+    instance_ssh_config_path: Union[Path, str],
+    documentdb_config_path: Union[Path, str],
+    database_config_path: Union[Path, str],
+):
+    instance_ssh_config = aws_infrastructure.tasks.ssh.SSHConfig.load(instance_ssh_config_path)
+    documentdb_config = scope.config.DocumentDBConfig.load(documentdb_config_path)
+    database_config = scope.config.DatabaseConfig.load(database_config_path)
+
+    @task
+    def populate(context, patients=10):
+        """
+        Populate the {} database with fake data.
+        """
+
+        with contextlib.ExitStack() as context_manager:
+            database = scope.documentdb.client.documentdb_client_database(
+                context_manager=context_manager,
+                instance_ssh_config=instance_ssh_config,
+                host=documentdb_config.endpoint,
+                port=documentdb_config.port,
+                direct_connection=True,
+                tls_insecure=True,
+                database_name=database_config.name,
+                user=database_config.user,
+                password=database_config.password,
+            )
+
+            scope.testing.fake_data.populate.populate_database(
+                database=database,
+                populate_patients=patients,
+            )
+
+    populate.__doc__ = populate.__doc__.format(database_config.name)
+
+    return populate
 
 
 def task_reset(
@@ -50,7 +170,7 @@ def task_reset(
         Reset and initialize the {} database.
         """
 
-        scope.database.initialize.reset(
+        _reset(
             instance_ssh_config=instance_ssh_config,
             documentdb_config=documentdb_config,
             database_config=database_config,
--- tasks/database.py
+++ tasks/database.py
@@ -18,6 +18,11 @@ ns_demo.add_task(scope.tasks.database.task_initialize(
     documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
     database_config_path=DEMO_DATABASE_CONFIG_PATH,
 ), "initialize")
+ns_demo.add_task(scope.tasks.database.task_populate(
+    instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
+    documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
+    database_config_path=DEMO_DATABASE_CONFIG_PATH,
+), "populate")
 ns_demo.add_task(scope.tasks.database.task_reset(
     instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
     documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
@@ -30,6 +35,11 @@ ns_dev.add_task(scope.tasks.database.task_initialize(
     documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
     database_config_path=DEV_DATABASE_CONFIG_PATH,
 ), "initialize")
+ns_dev.add_task(scope.tasks.database.task_populate(
+    instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
+    documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
+    database_config_path=DEV_DATABASE_CONFIG_PATH,
+), "populate")
 ns_dev.add_task(scope.tasks.database.task_reset(
     instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
     documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
