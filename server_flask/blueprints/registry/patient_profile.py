67be0859dc2d6158225e2582bfcec14c6180cbd5
James Fogarty <jayfo@users.noreply.github.com>
2022-01-30 10:19:05 -0800

    Add Flask Put with Tests

--- server_flask/blueprints/registry/patient_profile.py
+++ server_flask/blueprints/registry/patient_profile.py
@@ -1,18 +1,17 @@
+from flask import Blueprint, abort, current_app, jsonify, request
+from flask_json import as_json
 import http
-from functools import wraps
+import pymongo.errors
 
+from request_context import request_context
 import scope.database
 import scope.database.patient.patient_profile
 import scope.database.patients
-from flask import Blueprint, abort, current_app, jsonify, request
-from flask_json import as_json
-
-from request_context import request_context
 from scope.schema import patient_profile_schema
 from utils import validate_schema
 
 patient_profile_blueprint = Blueprint(
-    "registry_patient_profile_blueprint",
+    "patient_profile_blueprint",
     __name__,
 )
 
@@ -42,42 +41,61 @@ def get_patient_profile(patient_id):
     )
 
     # Retrieve the current patient profile
-    result = scope.database.patient.patient_profile.get_patient_profile(
+    document_retrieved = scope.database.patient.patient_profile.get_patient_profile(
         collection=patient_collection
     )
-    if patient_document is None:
-        # TODO: This is the correct semantics, evaluate impact on client.
+    if document_retrieved is None:
+        # TODO: This is the correct semantics, evaluate impact on client versus some kind of empty response.
+        #       Or should initialization of a patient include populating such documents.
         abort(http.HTTPStatus.NOT_FOUND)
 
-    return result
+    return document_retrieved
 
-#
-#
-# @registry_patient_profile_blueprint.route(
-#     "/<string:patient_collection>/profile", methods=["PUT"]
-# )
-# @validate_schema(patient_profile_schema)
-# @as_json
-# def update_patient_values(patient_collection):
-#
-#     patient_profile = request.json
-#
-#     if "_id" in patient_profile:
-#         abort(http.HTTPStatus.UNPROCESSABLE_ENTITY)  # 422
-#
-#     # Update _rev
-#     patient_profile["_rev"] += 1
-#
-#     context = request_context()
-#
-#     result = scope.database.patient.patient_profile.create_patient_profile(
-#         database=context.database,
-#         collection_name=patient_collection,
-#         patient_profile=patient_profile,
-#     )
-#
-#     if result is not None:
-#         return str(result.inserted_id), http.HTTPStatus.OK
-#     else:
-#         # NOTE: Send back the latest version of the document. Hold off on that.
-#         abort(http.HTTPStatus.UNPROCESSABLE_ENTITY)  # 422
+
+@patient_profile_blueprint.route(
+    "/<string:patient_id>/patient_profile", methods=["PUT"],
+)
+@validate_schema(patient_profile_schema)
+@as_json
+def put_patient_profile(patient_id):
+    # TODO: Require authentication
+
+    context = request_context()
+
+    # Use patient ID to confirm validity and obtain collection
+    # TODO: As part of authentication, determine if we can cache this.
+    #       Otherwise, maybe an annotation or enhance context to reduce code repetition.
+    patient_document = scope.database.patients.get_patient(
+        database=context.database,
+        patient_id=patient_id,
+    )
+    if patient_document is None:
+        abort(http.HTTPStatus.NOT_FOUND)
+
+    # Obtain patient collection
+    patient_collection = context.database.get_collection(
+        patient_document["collection"]
+    )
+
+    # Obtain the document being put
+    document = request.json
+
+    # Previously stored documents contain an "_id",
+    # documents to be put must not already contain an "_id"
+    if "_id" in document:
+        abort(http.HTTPStatus.BAD_REQUEST)
+
+    # Store the document
+    try:
+        result = scope.database.patient.patient_profile.put_patient_profile(
+            collection=patient_collection,
+            patient_profile=document
+        )
+
+        return result.document
+    except pymongo.errors.DuplicateKeyError as e:
+        # Indicates a race condition, return error and current revision to client
+        document_conflict = scope.database.patient.patient_profile.get_patient_profile(
+            collection=patient_collection
+        )
+        return document_conflict, http.HTTPStatus.CONFLICT
