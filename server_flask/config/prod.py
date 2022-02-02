3d339cc9157a861ac23d21bc34f82e601350a7a1
James Fogarty <jayfo@users.noreply.github.com>
2022-02-02 06:17:38 -0800

    Enable Production Configuration

--- server_flask/config/prod.py
+++ server_flask/config/prod.py
@@ -1,9 +1,23 @@
-import os
+from pathlib import Path
+from typing import Union
+
+import scope.config
+from config.base import Config
 
 
-class Config:
-    # TODO: Secret key will need to be shared across instances
-    SECRET_KEY = os.urandom(16)
+class ProductionConfig(Config):
+    def __init__(self, *, instance_dir: Union[Path, str]):
+        flask_config_path = Path(instance_dir, "flask_config.yaml")
+        flask_config = scope.config.FlaskConfig.load(flask_config_path)
 
-    # Database is in service `scope-database-svc-couchdb` on port 5984
-    URI_DATABASE = 'http://scope-database-svc-couchdb:5984/'
+        Config.__init__(
+            self=self,
+            secret_key=flask_config.secret_key,
+            documentdb_host=flask_config.documentdb_host,
+            documentdb_port=flask_config.documentdb_port,
+            documentdb_directconnection=flask_config.documentdb_directconnection,
+            documentdb_tlsinsecure=flask_config.documentdb_tlsinsecure,
+            database_name=flask_config.database_name,
+            database_user=flask_config.database_user,
+            database_password=flask_config.database_password,
+        )
