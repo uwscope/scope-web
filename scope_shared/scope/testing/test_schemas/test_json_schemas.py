9ae54a276855a885e926999827f79761e78f33c1
Anant Mittal <anmittal@cs.washington.edu>
2022-02-02 16:57:54 -0800

    Added more json schema tests.

--- scope_shared/scope/testing/test_schemas/test_json_schemas.py
+++ scope_shared/scope/testing/test_schemas/test_json_schemas.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
-import jschon
 from pathlib import Path
 from pprint import pprint
-import pytest
 from typing import Union
 
+import jschon
+import pytest
 import scope.schema
 
 JSON_DATA_PATH = Path(Path(__file__).parent, "json")
@@ -19,36 +19,66 @@ class ConfigTestJSONSchema:
 
 
 TEST_CONFIGS = [
+    # clinicalHistory
     ConfigTestJSONSchema(
         name="clinical-history",
         schema=scope.schema.clinical_history_schema,
-        document_path="clinical-history.json",
+        document_path="clinical-history/valid/all-properties.json",
         expected_valid=True,
     ),
+    ConfigTestJSONSchema(
+        name="clinical-history",
+        schema=scope.schema.clinical_history_schema,
+        document_path="clinical-history/invalid/bad-date-dateOfCancerDiagnosis.json",
+        expected_valid=False,
+    ),
+    # identity
     ConfigTestJSONSchema(
         name="identity",
         schema=scope.schema.identity_schema,
         document_path="identity.json",
         expected_valid=True,
     ),
+    # patient
     ConfigTestJSONSchema(
         name="patient",
         schema=scope.schema.patient_schema,
         document_path="patient.json",
         expected_valid=True,
     ),
+    # patientProfile
     ConfigTestJSONSchema(
         name="patient-profile",
         schema=scope.schema.patient_profile_schema,
         document_path="patient-profile.json",
         expected_valid=True,
     ),
+    # safetyPlan
     ConfigTestJSONSchema(
         name="safety-plan",
         schema=scope.schema.safety_plan_schema,
-        document_path="safety-plan.json",
+        document_path="safety-plan/valid/some-properties.json",
         expected_valid=True,
     ),
+    ConfigTestJSONSchema(
+        name="safety-plan",
+        schema=scope.schema.safety_plan_schema,
+        document_path="safety-plan/valid/distractions-mix-string-and-contact.json",
+        expected_valid=True,
+    ),
+    ConfigTestJSONSchema(
+        name="safety-plan",
+        schema=scope.schema.safety_plan_schema,
+        document_path="safety-plan/valid/distractions-string-array.json",
+        expected_valid=True,
+    ),
+    ConfigTestJSONSchema(
+        name="safety-plan",
+        schema=scope.schema.safety_plan_schema,
+        document_path="safety-plan/valid/distractions-contact-array.json",
+        expected_valid=True,
+    ),
+    # sessions
     ConfigTestJSONSchema(
         name="session",
         schema=scope.schema.session_schema,
@@ -61,6 +91,7 @@ TEST_CONFIGS = [
         document_path="sessions.json",
         expected_valid=True,
     ),
+    # valuesInventory
     ConfigTestJSONSchema(
         name="values-inventory",
         schema=scope.schema.values_inventory_schema,
