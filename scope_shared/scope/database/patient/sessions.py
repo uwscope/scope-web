d0e39cb71309f1428752ea0644134fb31c463cb1
Anant Mittal <anmittal@cs.washington.edu>
2022-02-10 00:56:51 -0800

    Updated sessions model files

--- scope_shared/scope/database/patient/sessions.py
+++ scope_shared/scope/database/patient/sessions.py
@@ -1,100 +1,73 @@
-import hashlib
-import re
 from typing import List, Optional
 
-import bson
 import pymongo
-import pymongo.database
-import pymongo.errors
-import pymongo.results
+import pymongo.collection
+import scope.database.collection_utils
 
 DOCUMENT_TYPE = "session"
+DOCUMENT_ID = "sessionId"
 
 
 def get_sessions(
-    *, database: pymongo.database.Database, collection_name: str
-) -> Optional[list]:
+    *,
+    collection: pymongo.collection.Collection,
+) -> Optional[List[dict]]:
     """
     Retrieve list of "session" document.
     """
 
-    collection = database.get_collection(name=collection_name)
-
-    # Find unique session ids and then get document with latest _rev from them.
-    pipeline = [
-        {"$match": {"_type": "session"}},
-        {"$sort": {"_rev": pymongo.DESCENDING}},
-        {
-            "$group": {
-                "_id": "$_session_id",
-                "latest_session_document": {"$first": "$$ROOT"},
-            }
-        },
-        {"$replaceRoot": {"newRoot": "$latest_session_document"}},
-    ]
-
-    found_sessions = list(collection.aggregate(pipeline))
-    if found_sessions is not None:
-        for found_session in found_sessions:
-            if "_id" in found_session:
-                found_session["_id"] = str(found_session["_id"])
-
-    return found_sessions
+    return scope.database.collection_utils.get_set(
+        collection=collection,
+        document_type=DOCUMENT_TYPE,
+    )
 
 
 def get_session(
-    *, database: pymongo.database.Database, collection_name: str, session_id: str
-) -> Optional[list]:
+    *,
+    collection: pymongo.collection.Collection,
+    set_id: str,
+) -> Optional[dict]:
     """
     Retrieve "session" document.
     """
 
-    collection = database.get_collection(name=collection_name)
-
-    query = {"_type": "session", "_session_id": session_id}
-
-    # Find the document with highest `_rev`.
-    session = collection.find_one(filter=query, sort=[("_rev", pymongo.DESCENDING)])
-
-    if "_id" in session:
-        session["_id"] = str(session["_id"])
-    # TODO: Verify schema against session json.
-
-    return session
+    return scope.database.collection_utils.get_set_element(
+        collection=collection,
+        document_type=DOCUMENT_TYPE,
+        set_id=set_id,
+    )
 
 
-def create_session(
-    *, database: pymongo.database.Database, collection_name: str, session: dict
-):
+def post_session(
+    *,
+    collection: pymongo.collection.Collection,
+    session: dict,
+) -> scope.database.collection_utils.PutResult:
     """
     Create the "session" document.
     """
 
-    collection = database.get_collection(name=collection_name)
-
-    # Make sure _session_id does not already exist.
-    query = {"_type": "session", "_session_id": session["_session_id"]}
-    if collection.find_one(filter=query) is None:
-        try:
-            result = collection.insert_one(document=session)
-            return result
-        except pymongo.errors.DuplicateKeyError:
-            return None
-    else:
-        return None
+    return scope.database.collection_utils.put_set_element(
+        collection=collection,
+        document_type=DOCUMENT_TYPE,
+        set_id=session[DOCUMENT_ID],
+        document=session,
+    )
 
 
-def update_session(
-    *, database: pymongo.database.Database, collection_name: str, session: dict
+def put_session(
+    *,
+    collection: pymongo.collection.Collection,
+    session: dict,
 ):
     """
-    Update (insert) the "session" document.
+    Update the "session" document.
     """
+    # NOTE: Exactly same as post_session, but keeping it here if we need additional computation.
 
-    collection = database.get_collection(name=collection_name)
-
-    try:
-        result = collection.insert_one(document=session)
-        return result
-    except pymongo.errors.DuplicateKeyError:
-        return None
+    return scope.database.collection_utils.put_set_element(
+        collection=collection,
+        document_type=DOCUMENT_TYPE,
+        set_id=session[DOCUMENT_ID],
+        document=session,
+    )
