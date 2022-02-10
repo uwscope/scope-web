9c1785a79f2bde4993d260f5e5379db9e96baa38
Anant Mittal <anmittal@cs.washington.edu>
2022-02-09 20:24:30 -0800

    Updated TEST_CONFIGS in test_fake_data_schemas.py

--- scope_shared/scope/testing/test_schemas/test_fake_data_schemas.py
+++ scope_shared/scope/testing/test_schemas/test_fake_data_schemas.py
@@ -7,11 +7,17 @@ import jschon
 import pytest
 import scope.schema
 import scope.testing.fake_data.fixtures_fake_activity
+import scope.testing.fake_data.fixtures_fake_case_review
+import scope.testing.fake_data.fixtures_fake_case_reviews
 import scope.testing.fake_data.fixtures_fake_clinical_history
 import scope.testing.fake_data.fixtures_fake_contact
+import scope.testing.fake_data.fixtures_fake_identity
 import scope.testing.fake_data.fixtures_fake_life_areas
 import scope.testing.fake_data.fixtures_fake_patient_profile
+import scope.testing.fake_data.fixtures_fake_referral_status
 import scope.testing.fake_data.fixtures_fake_safety_plan
+import scope.testing.fake_data.fixtures_fake_session
+import scope.testing.fake_data.fixtures_fake_sessions
 import scope.testing.fake_data.fixtures_fake_values_inventory
 
 
@@ -32,11 +38,33 @@ faker_factory = faker.Faker()
 
 TEST_CONFIGS = [
     ConfigTestFakeDataSchema(
+        name="identity",
+        schema=scope.schema.identity_schema,
+        data_factory=scope.testing.fake_data.fixtures_fake_identity.fake_identity_factory(
+            faker_factory=faker_factory,
+        ),
+        expected_valid=True,
+    ),
+    ConfigTestFakeDataSchema(
         name="contact",
         schema=scope.schema.contact_schema,
         data_factory=scope.testing.fake_data.fixtures_fake_contact.fake_contact_factory(
             faker_factory=faker_factory,
         ),
+        expected_valid=True,
+    ),
+    ConfigTestFakeDataSchema(
+        name="referral-status",
+        schema=scope.schema.referral_status_schema,
+        data_factory=scope.testing.fake_data.fixtures_fake_referral_status.fake_referral_status_factory(
+            faker_factory=faker_factory,
+        ),
+        expected_valid=True,
+    ),
+    ConfigTestFakeDataSchema(
+        name="life-areas",
+        schema=scope.schema.life_areas_schema,
+        data_factory=scope.testing.fake_data.fixtures_fake_life_areas.fake_life_areas_factory(),
         expected_valid=True,
     ),
     ConfigTestFakeDataSchema(
@@ -56,6 +84,15 @@ TEST_CONFIGS = [
         expected_valid=True,
     ),
     ConfigTestFakeDataSchema(
+        name="values-inventory",
+        schema=scope.schema.values_inventory_schema,
+        data_factory=scope.testing.fake_data.fixtures_fake_values_inventory.fake_values_inventory_factory(
+            faker_factory=faker_factory,
+            fake_life_areas=scope.testing.fake_data.fixtures_fake_life_areas.fake_life_areas_factory()(),
+        ),
+        expected_valid=True,
+    ),
+    ConfigTestFakeDataSchema(
         name="safety-plan",
         schema=scope.schema.safety_plan_schema,
         data_factory=scope.testing.fake_data.fixtures_fake_safety_plan.fake_safety_plan_factory(
@@ -67,31 +104,53 @@ TEST_CONFIGS = [
         expected_valid=True,
     ),
     ConfigTestFakeDataSchema(
+        name="session",
+        schema=scope.schema.session_schema,
+        data_factory=scope.testing.fake_data.fixtures_fake_session.fake_session_factory(
+            faker_factory=faker_factory,
+            fake_referral_status_factory=scope.testing.fake_data.fixtures_fake_referral_status.fake_referral_status_factory(
+                faker_factory=faker_factory,
+            ),
+        ),
+        expected_valid=True,
+    ),
+    ConfigTestFakeDataSchema(
+        name="sessions",
+        schema=scope.schema.sessions_schema,
+        data_factory=scope.testing.fake_data.fixtures_fake_sessions.fake_sessions_factory(
+            fake_session_factory=scope.testing.fake_data.fixtures_fake_session.fake_session_factory(
+                faker_factory=faker_factory,
+                fake_referral_status_factory=scope.testing.fake_data.fixtures_fake_referral_status.fake_referral_status_factory(
+                    faker_factory=faker_factory,
+                ),
+            ),
+        ),
+        expected_valid=True,
+    ),
+    ConfigTestFakeDataSchema(
+        name="case-review",
+        schema=scope.schema.case_review_schema,
+        data_factory=scope.testing.fake_data.fixtures_fake_case_review.fake_case_review_factory(
+            faker_factory=faker_factory,
+        ),
+        expected_valid=True,
+    ),
+    ConfigTestFakeDataSchema(
+        name="case-reviews",
+        schema=scope.schema.case_reviews_schema,
+        data_factory=scope.testing.fake_data.fixtures_fake_case_reviews.fake_case_reviews_factory(
+            fake_case_review_factory=scope.testing.fake_data.fixtures_fake_case_review.fake_case_review_factory(
+                faker_factory=faker_factory,
+            ),
+        ),
+        expected_valid=True,
+    ),
+    ConfigTestFakeDataSchema(
         # TODO: what schema applies here?
         XFAIL_TEST_HAS_TODO=True,
         name="activity",
         schema=scope.schema.activity_schema,
         data_factory=scope.testing.fake_data.fixtures_fake_activity.data_fake_activity_factory,
-        expected_valid=True,
-    ),
-    ConfigTestFakeDataSchema(
-        # TODO: There currently is no schema defined for life areas. I think:
-        #       - life-area.json should describe one of the documents found in server_flask/app_config/life_areas.
-        #       - life-areas.json should describe a list of such documents.
-        XFAIL_TEST_HAS_TODO=True,
-        name="life-areas",
-        # TODO: This should then be life_areas_schema
-        schema=scope.schema.values_inventory_schema,
-        data_factory=scope.testing.fake_data.fixtures_fake_life_areas.fake_life_areas_factory(),
-        expected_valid=True,
-    ),
-    ConfigTestFakeDataSchema(
-        name="values-inventory",
-        schema=scope.schema.values_inventory_schema,
-        data_factory=scope.testing.fake_data.fixtures_fake_values_inventory.fake_values_inventory_factory(
-            faker_factory=faker_factory,
-            fake_life_areas=scope.testing.fake_data.fixtures_fake_life_areas.fake_life_areas_factory()(),
-        ),
         expected_valid=True,
     ),
 ]
