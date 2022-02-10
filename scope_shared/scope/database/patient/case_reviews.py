86e92564a2ea1d103790f6a0bba45e2b44c73015
Anant Mittal <anmittal@cs.washington.edu>
2022-02-10 00:56:29 -0800

    Updated case reviews model file

--- scope_shared/scope/database/patient/case_reviews.py
+++ scope_shared/scope/database/patient/case_reviews.py
@@ -1,100 +1,73 @@
-import hashlib
-import re
 from typing import List, Optional
 
-import bson
 import pymongo
-import pymongo.database
-import pymongo.errors
-import pymongo.results
+import pymongo.collection
+import scope.database.collection_utils
 
 DOCUMENT_TYPE = "caseReview"
+DOCUMENT_ID = "reviewId"
 
 
 def get_case_reviews(
-    *, database: pymongo.database.Database, collection_name: str
-) -> Optional[list]:
+    *,
+    collection: pymongo.collection.Collection,
+) -> Optional[List[dict]]:
     """
     Retrieve list of "caseReview" document.
     """
 
-    collection = database.get_collection(name=collection_name)
-
-    # Find unique review ids and then get document with latest _rev from them.
-    pipeline = [
-        {"$match": {"_type": DOCUMENT_TYPE}},
-        {"$sort": {"_rev": pymongo.DESCENDING}},
-        {
-            "$group": {
-                "_id": "$_review_id",
-                "latest_case_review_document": {"$first": "$$ROOT"},
-            }
-        },
-        {"$replaceRoot": {"newRoot": "$latest_case_review_document"}},
-    ]
-
-    found_case_reviews = list(collection.aggregate(pipeline))
-    if found_case_reviews is not None:
-        for fcr in found_case_reviews:
-            if "_id" in fcr:
-                fcr["_id"] = str(fcr["_id"])
-
-    return found_case_reviews
+    return scope.database.collection_utils.get_set(
+        collection=collection,
+        document_type=DOCUMENT_TYPE,
+    )
 
 
 def get_case_review(
-    *, database: pymongo.database.Database, collection_name: str, review_id: str
-) -> Optional[list]:
+    *,
+    collection: pymongo.collection.Collection,
+    set_id: str,
+) -> Optional[dict]:
     """
     Retrieve "caseReview" document.
     """
 
-    collection = database.get_collection(name=collection_name)
-
-    query = {"_type": DOCUMENT_TYPE, "_review_id": review_id}
-
-    # Find the document with highest `_rev`.
-    case_review = collection.find_one(filter=query, sort=[("_rev", pymongo.DESCENDING)])
-
-    if "_id" in case_review:
-        case_review["_id"] = str(case_review["_id"])
-    # TODO: Verify schema against case review json.
-
-    return case_review
+    return scope.database.collection_utils.get_set_element(
+        collection=collection,
+        document_type=DOCUMENT_TYPE,
+        set_id=set_id,
+    )
 
 
-def create_case_review(
-    *, database: pymongo.database.Database, collection_name: str, case_review: dict
-):
+def post_case_review(
+    *,
+    collection: pymongo.collection.Collection,
+    case_review: dict,
+) -> scope.database.collection_utils.PutResult:
     """
     Create the "caseReview" document.
     """
 
-    collection = database.get_collection(name=collection_name)
-
-    # Make sure _review_id does not already exist.
-    query = {"_type": DOCUMENT_TYPE, "_review_id": case_review["_review_id"]}
-    if collection.find_one(filter=query) is None:
-        try:
-            result = collection.insert_one(document=case_review)
-            return result
-        except pymongo.errors.DuplicateKeyError:
-            return None
-    else:
-        return None
+    return scope.database.collection_utils.put_set_element(
+        collection=collection,
+        document_type=DOCUMENT_TYPE,
+        set_id=case_review[DOCUMENT_ID],
+        document=case_review,
+    )
 
 
-def update_case_review(
-    *, database: pymongo.database.Database, collection_name: str, case_review: dict
+def put_case_review(
+    *,
+    collection: pymongo.collection.Collection,
+    case_review: dict,
 ):
     """
-    Update (insert) the "caseReview" document.
+    Update the "caseReview" document.
     """
+    # NOTE: Exactly same as post_case_review, but keeping it here if we need additional computation.
 
-    collection = database.get_collection(name=collection_name)
-
-    try:
-        result = collection.insert_one(document=case_review)
-        return result
-    except pymongo.errors.DuplicateKeyError:
-        return None
+    return scope.database.collection_utils.put_set_element(
+        collection=collection,
+        document_type=DOCUMENT_TYPE,
+        set_id=case_review[DOCUMENT_ID],
+        document=case_review,
+    )
