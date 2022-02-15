5cb3f83ea5172dbd42fcecde1adba03fd9383e3c
Jina Suh <jasmart@gmail.com>
2022-02-14 19:54:17 -0800

    Fix list with icons

--- web_patient/src/components/Progress/ProgressPage.tsx
+++ web_patient/src/components/Progress/ProgressPage.tsx
@@ -1,8 +1,9 @@
-import { List, ListItem } from '@mui/material';
+import { Divider, List, ListItem } from '@mui/material';
 import React, { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
 import ListCard from 'src/components/common/ListCard';
 import { MainPage } from 'src/components/common/MainPage';
+import Section from 'src/components/common/Section';
 import { getImage } from 'src/services/images';
 import { Routes } from 'src/services/routes';
 import { getString } from 'src/services/strings';
@@ -10,26 +11,30 @@ import { getString } from 'src/services/strings';
 export const ProgressPage: FunctionComponent = () => {
     return (
         <MainPage title={getString('Navigation_progress')}>
-            <List>
-                <ListItem disableGutters={true} component={Link} to={Routes.phqProgress}>
-                    <ListCard
-                        title={getString('Progress_phq_assessment_title')}
-                        imageSrc={getImage('Resources_values_button_image')}
-                    />
-                </ListItem>
-                <ListItem disableGutters={true} component={Link} to={Routes.gadProgress}>
-                    <ListCard
-                        title={getString('Progress_gad_assessment_title')}
-                        imageSrc={getImage('Resources_values_button_image')}
-                    />
-                </ListItem>
-                <ListItem disableGutters={true} component={Link} to={Routes.activityProgress}>
-                    <ListCard
-                        title={getString('Progress_activity_tracking_title')}
-                        imageSrc={getImage('Progress_activity_button_image')}
-                    />
-                </ListItem>
-            </List>
+            <Section>
+                <List>
+                    <ListItem disableGutters={true} component={Link} to={Routes.phqProgress}>
+                        <ListCard
+                            title={getString('Progress_phq_assessment_title')}
+                            imageSrc={getImage('Resources_values_button_image')}
+                        />
+                    </ListItem>
+                    <Divider />
+                    <ListItem disableGutters={true} component={Link} to={Routes.gadProgress}>
+                        <ListCard
+                            title={getString('Progress_gad_assessment_title')}
+                            imageSrc={getImage('Resources_values_button_image')}
+                        />
+                    </ListItem>
+                    <Divider />
+                    <ListItem disableGutters={true} component={Link} to={Routes.activityProgress}>
+                        <ListCard
+                            title={getString('Progress_activity_tracking_title')}
+                            imageSrc={getImage('Progress_activity_button_image')}
+                        />
+                    </ListItem>
+                </List>
+            </Section>
         </MainPage>
     );
 };
--- web_patient/src/components/Resources/ResourcesPage.tsx
+++ web_patient/src/components/Resources/ResourcesPage.tsx
@@ -1,8 +1,9 @@
-import { List, ListItem } from '@mui/material';
+import { Divider, List, ListItem } from '@mui/material';
 import React, { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
 import ListCard from 'src/components/common/ListCard';
 import { MainPage } from 'src/components/common/MainPage';
+import Section from 'src/components/common/Section';
 import { getImage } from 'src/services/images';
 import { getFormPath, ParameterValues, Routes } from 'src/services/routes';
 import { getString } from 'src/services/strings';
@@ -10,29 +11,41 @@ import { getString } from 'src/services/strings';
 export const ResourcesPage: FunctionComponent = () => {
     return (
         <MainPage title={getString('Navigation_resources')}>
-            <List>
-                <ListItem disableGutters={true} component={Link} to={Routes.valuesInventory}>
-                    <ListCard
-                        title={getString('Resources_inventory_title')}
-                        subtitle={getString('Resources_inventory_subtitle')}
-                        imageSrc={getImage('Resources_values_button_image')}
-                    />
-                </ListItem>
-                <ListItem disableGutters={true} component={Link} to={Routes.worksheets}>
-                    <ListCard
-                        title={getString('Resources_resources_title')}
-                        subtitle={getString('Resources_resources_subtitle')}
-                        imageSrc={getImage('Resources_worksheets_button_image')}
-                    />
-                </ListItem>
-                <ListItem disableGutters={true} component={Link} to={getFormPath(ParameterValues.form.safetyPlan)}>
-                    <ListCard
-                        title={getString('Resources_safety_plan_title')}
-                        subtitle={getString('Resources_safety_plan_subtitle')}
-                        imageSrc={getImage('Resources_safety_button_image')}
-                    />
-                </ListItem>
-            </List>
+            <Section>
+                <List>
+                    <ListItem
+                        alignItems="flex-start"
+                        disableGutters={true}
+                        component={Link}
+                        to={Routes.valuesInventory}>
+                        <ListCard
+                            title={getString('Resources_inventory_title')}
+                            subtitle={getString('Resources_inventory_subtitle')}
+                            imageSrc={getImage('Resources_values_button_image')}
+                        />
+                    </ListItem>
+                    <Divider />
+                    <ListItem alignItems="flex-start" disableGutters={true} component={Link} to={Routes.worksheets}>
+                        <ListCard
+                            title={getString('Resources_resources_title')}
+                            subtitle={getString('Resources_resources_subtitle')}
+                            imageSrc={getImage('Resources_worksheets_button_image')}
+                        />
+                    </ListItem>
+                    <Divider />
+                    <ListItem
+                        alignItems="flex-start"
+                        disableGutters={true}
+                        component={Link}
+                        to={getFormPath(ParameterValues.form.safetyPlan)}>
+                        <ListCard
+                            title={getString('Resources_safety_plan_title')}
+                            subtitle={getString('Resources_safety_plan_subtitle')}
+                            imageSrc={getImage('Resources_safety_button_image')}
+                        />
+                    </ListItem>
+                </List>
+            </Section>
         </MainPage>
     );
 };
