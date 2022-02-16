6dc6a9756fca19acc0348565ad8a9f333c427ad5
Jina Suh <jasmart@gmail.com>
2022-02-15 20:46:55 -0800

    Values inventory schema changes

--- web_shared/serviceTypes.ts
+++ web_shared/serviceTypes.ts
@@ -1,6 +1,11 @@
-import { IPatient } from 'shared/types';
+import { IPatient, IValuesInventory } from 'shared/types';
 
 export interface IPatientListResponse {
     message: string;
     patients: IPatient[];
+}
+
+export interface IValuesInventoryResponse {
+    status: number;
+    valuesinventory: IValuesInventory;
 }
