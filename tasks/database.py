90c2c035d6d4e60563e8fd769eb659396f665d1d
James Fogarty <jayfo@users.noreply.github.com>
2022-02-16 16:17:00 -0800

    Make Database Tasks Optional

--- tasks/database.py
+++ tasks/database.py
@@ -1,4 +1,5 @@
 from aws_infrastructure.tasks.collection import compose_collection
+from pathlib import Path
 from invoke import Collection
 
 import scope.tasks.database
@@ -12,39 +13,42 @@ DATABASE_DEV_CONFIG_PATH = "./secrets/configuration/database_dev.yaml"
 # Build task collection
 ns = Collection("database")
 
-ns_demo = Collection("demo")
-ns_demo.add_task(scope.tasks.database.task_initialize(
-    instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
-    documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
-    database_config_path=DATABASE_DEMO_CONFIG_PATH,
-), "initialize")
-ns_demo.add_task(scope.tasks.database.task_populate(
-    instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
-    documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
-    database_config_path=DATABASE_DEMO_CONFIG_PATH,
-), "populate")
-ns_demo.add_task(scope.tasks.database.task_reset(
-    instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
-    documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
-    database_config_path=DATABASE_DEMO_CONFIG_PATH,
-), "reset")
+if Path(DATABASE_DEMO_CONFIG_PATH).exists():
+    ns_demo = Collection("demo")
+    ns_demo.add_task(scope.tasks.database.task_initialize(
+        instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
+        documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
+        database_config_path=DATABASE_DEMO_CONFIG_PATH,
+    ), "initialize")
+    ns_demo.add_task(scope.tasks.database.task_populate(
+        instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
+        documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
+        database_config_path=DATABASE_DEMO_CONFIG_PATH,
+    ), "populate")
+    ns_demo.add_task(scope.tasks.database.task_reset(
+        instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
+        documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
+        database_config_path=DATABASE_DEMO_CONFIG_PATH,
+    ), "reset")
 
-ns_dev = Collection("dev")
-ns_dev.add_task(scope.tasks.database.task_initialize(
-    instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
-    documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
-    database_config_path=DATABASE_DEV_CONFIG_PATH,
-), "initialize")
-ns_dev.add_task(scope.tasks.database.task_populate(
-    instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
-    documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
-    database_config_path=DATABASE_DEV_CONFIG_PATH,
-), "populate")
-ns_dev.add_task(scope.tasks.database.task_reset(
-    instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
-    documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
-    database_config_path=DATABASE_DEV_CONFIG_PATH,
-), "reset")
+    compose_collection(ns, ns_demo, name="demo")
 
-compose_collection(ns, ns_demo, name="demo")
-compose_collection(ns, ns_dev, name="dev")
+if Path(DATABASE_DEV_CONFIG_PATH).exists():
+    ns_dev = Collection("dev")
+    ns_dev.add_task(scope.tasks.database.task_initialize(
+        instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
+        documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
+        database_config_path=DATABASE_DEV_CONFIG_PATH,
+    ), "initialize")
+    ns_dev.add_task(scope.tasks.database.task_populate(
+        instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
+        documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
+        database_config_path=DATABASE_DEV_CONFIG_PATH,
+    ), "populate")
+    ns_dev.add_task(scope.tasks.database.task_reset(
+        instance_ssh_config_path=INSTANCE_SSH_CONFIG_PATH,
+        documentdb_config_path=DOCUMENTDB_CONFIG_PATH,
+        database_config_path=DATABASE_DEV_CONFIG_PATH,
+    ), "reset")
+
+    compose_collection(ns, ns_dev, name="dev")
