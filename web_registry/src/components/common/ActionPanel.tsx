062c2137ee652d9869e0c7b8a1541cc4c94c013b
Jina Suh <jasmart@gmail.com>
2022-02-16 20:20:28 -0800

    Show error on service call failure

--- web_registry/src/components/common/ActionPanel.tsx
+++ web_registry/src/components/common/ActionPanel.tsx
@@ -1,13 +1,23 @@
-import { Button, Card, CardActions, CardContent, Divider, Grid, LinearProgress, Typography } from '@mui/material';
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Divider,
+    Grid,
+    LinearProgress,
+    Snackbar,
+    Typography,
+} from '@mui/material';
 import withTheme from '@mui/styles/withTheme';
-import React, { FunctionComponent } from 'react';
+import React, { Fragment, FunctionComponent, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const CardTitle = withTheme(
     styled.div((props) => ({
         height: 42,
         padding: props.theme.spacing(2),
-    }))
+    })),
 );
 
 const TitleType = styled(Typography)({});
@@ -18,7 +28,7 @@ const InlineType = withTheme(
         fontWeight: 400,
         color: 'rgba(0, 0, 0, 0.54)',
         paddingLeft: props.theme.spacing(1),
-    }))
+    })),
 );
 
 const Loading = withTheme(styled(LinearProgress)({ height: 1 }));
@@ -36,45 +46,65 @@ export interface IActionPanelProps {
     children?: React.ReactNode;
     id: string;
     loading?: boolean;
+    error?: boolean;
 }
 
 export const ActionPanel: FunctionComponent<IActionPanelProps> = (props) => {
-    const { id, actionButtons, title, inlineTitle, children, loading = false } = props;
+    const { id, actionButtons, title, inlineTitle, children, loading = false, error = false } = props;
+
+    const [openError, setOpenError] = useState(error);
+
+    useEffect(() => {
+        setOpenError(error);
+    }, [error]);
+
+    const handleClose = () => {
+        setOpenError(false);
+    };
+
     return (
-        <Card id={id}>
-            <CardTitle>
-                <Grid container spacing={2} direction="row" justifyContent="space-between" alignItems="center">
-                    <Grid item p={0}>
-                        <TitleType variant="h6" noWrap={true}>
-                            {title}
-                        </TitleType>
+        <Fragment>
+            <Card id={id}>
+                <CardTitle>
+                    <Grid container spacing={2} direction="row" justifyContent="space-between" alignItems="center">
+                        <Grid item p={0}>
+                            <TitleType variant="h6" noWrap={true}>
+                                {title}
+                            </TitleType>
+                        </Grid>
+                        <Grid item flexGrow={1}>
+                            {inlineTitle && <InlineType>{`(${inlineTitle})`}</InlineType>}
+                        </Grid>
+                        <Grid item>
+                            <CardActions sx={{ padding: 0, minHeight: 28 }}>
+                                {!!actionButtons
+                                    ? actionButtons.map((a) => (
+                                          <Button
+                                              variant="outlined"
+                                              size="small"
+                                              color="primary"
+                                              startIcon={a.icon}
+                                              disabled={loading || !a.onClick}
+                                              onClick={a.onClick}
+                                              key={a.text}>
+                                              {a.text}
+                                          </Button>
+                                      ))
+                                    : null}
+                            </CardActions>
+                        </Grid>
                     </Grid>
-                    <Grid item flexGrow={1}>
-                        {inlineTitle && <InlineType>{`(${inlineTitle})`}</InlineType>}
-                    </Grid>
-                    <Grid item>
-                        <CardActions sx={{ padding: 0, minHeight: 28 }}>
-                            {!!actionButtons
-                                ? actionButtons.map((a) => (
-                                      <Button
-                                          variant="outlined"
-                                          size="small"
-                                          color="primary"
-                                          startIcon={a.icon}
-                                          disabled={loading || !a.onClick}
-                                          onClick={a.onClick}
-                                          key={a.text}>
-                                          {a.text}
-                                      </Button>
-                                  ))
-                                : null}
-                        </CardActions>
-                    </Grid>
-                </Grid>
-            </CardTitle>
-            {loading ? <Loading /> : <Divider variant="middle" />}
-            <CardContent sx={{ padding: 3 }}>{children}</CardContent>
-        </Card>
+                </CardTitle>
+                {loading ? <Loading /> : <Divider variant="middle" />}
+                <CardContent sx={{ padding: 3 }}>{children}</CardContent>
+            </Card>
+            <Snackbar
+                open={openError}
+                message={`Sorry, there was an error processing your request. Please try again.`}
+                autoHideDuration={2000}
+                onClose={handleClose}
+            />
+        </Fragment>
     );
 };
 
