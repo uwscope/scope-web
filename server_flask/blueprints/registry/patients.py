0f342e6d0a2de558787cbb667bef22aa2c6b90ea
James Fogarty <jayfo@users.noreply.github.com>
2022-02-16 16:52:54 -0800

    Pull Empty Fake from Database

--- server_flask/blueprints/registry/patients.py
+++ server_flask/blueprints/registry/patients.py
@@ -34,14 +34,20 @@ def _frankenfake_document(
     patient_collection: pymongo.collection.Collection,
 ) -> dict:
     if patient_id in FAKE_EMPTY_PATIENTS:
+        profile = scope.database.patient.patient_profile.get_patient_profile(
+            collection=patient_collection
+        )
+        if profile is None:
+            profile = fake_document["profile"]
+
         result_document = {
             "identity": {
                 "identityId": fake_document["identity"]["identityId"],
-                "name": fake_document["profile"]["name"],
+                "name": profile["name"],
             },
             "profile": {
-                "name": fake_document["profile"]["name"],
-                "MRN": fake_document["profile"]["MRN"],
+                "name": profile["name"],
+                "MRN": profile["MRN"],
             }
         }
     else:
