18b83b310a1a02bc274178b1c4d2aef9d5c7400b
James Fogarty <jayfo@users.noreply.github.com>
2022-02-21 07:18:45 -0800

    Test with and without database fields

--- scope_shared/scope/testing/test_schemas/test_fake_data_schemas.py
+++ scope_shared/scope/testing/test_schemas/test_fake_data_schemas.py
@@ -1,10 +1,16 @@
-from dataclasses import dataclass
-from pprint import pprint
-from typing import Callable, Union
+import copy
 
+import bson.objectid
+from dataclasses import dataclass
 import faker
 import jschon
+from pprint import pprint
 import pytest
+from typing import Callable, List, Optional, Union
+
+import scope.database.patient.case_reviews
+import scope.database.patient.sessions
+import scope.database.collection_utils as collection_utils
 import scope.database.document_utils as document_utils
 import scope.schema
 import scope.testing.fake_data.fixtures_fake_activity
@@ -26,8 +32,12 @@ import scope.testing.fake_data.fixtures_fake_values_inventory
 class ConfigTestFakeDataSchema:
     name: str
     schema: jschon.JSONSchema
-    data_factory: Callable[[], Union[dict, list]]
-    expected_valid: bool
+    data_factory: Callable[[], Union[dict, List[dict]]]
+
+    expected_document: bool
+    expected_singleton: bool
+    expected_set_element: bool
+    expected_semantic_set_id: Optional[str]
 
     # Used to indicate test is not resolved
     # because it has an issue in data or schema
@@ -44,7 +54,10 @@ TEST_CONFIGS = [
         name="activity",
         schema=scope.schema.activity_schema,
         data_factory=scope.testing.fake_data.fixtures_fake_activity.data_fake_activity_factory,
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=True,
+        expected_set_element=True,
+        expected_semantic_set_id="TODO",
     ),
     ConfigTestFakeDataSchema(
         name="case-review",
@@ -52,7 +65,10 @@ TEST_CONFIGS = [
         data_factory=scope.testing.fake_data.fixtures_fake_case_review.fake_case_review_factory(
             faker_factory=faker_factory,
         ),
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=False,
+        expected_set_element=True,
+        expected_semantic_set_id=scope.database.patient.case_reviews.SEMANTIC_SET_ID,
     ),
     ConfigTestFakeDataSchema(
         name="case-reviews",
@@ -62,7 +78,10 @@ TEST_CONFIGS = [
                 faker_factory=faker_factory,
             ),
         ),
-        expected_valid=True,
+        expected_document=False,
+        expected_singleton=False,
+        expected_set_element=False,
+        expected_semantic_set_id=None,
     ),
     ConfigTestFakeDataSchema(
         name="clinical-history",
@@ -70,7 +89,10 @@ TEST_CONFIGS = [
         data_factory=scope.testing.fake_data.fixtures_fake_clinical_history.fake_clinical_history_factory(
             faker_factory=faker_factory,
         ),
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=True,
+        expected_set_element=False,
+        expected_semantic_set_id=None,
     ),
     ConfigTestFakeDataSchema(
         name="contact",
@@ -78,7 +100,10 @@ TEST_CONFIGS = [
         data_factory=scope.testing.fake_data.fixtures_fake_contact.fake_contact_factory(
             faker_factory=faker_factory,
         ),
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=False,
+        expected_set_element=False,
+        expected_semantic_set_id=None,
     ),
     ConfigTestFakeDataSchema(
         name="identity",
@@ -86,13 +111,21 @@ TEST_CONFIGS = [
         data_factory=scope.testing.fake_data.fixtures_fake_identity.fake_identity_factory(
             faker_factory=faker_factory,
         ),
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=False,
+        expected_set_element=True,
+        # TODO: Define the semantic IDs
+        XFAIL_TEST_HAS_TODO=True,
+        expected_semantic_set_id=None,
     ),
     ConfigTestFakeDataSchema(
         name="life-areas",
         schema=scope.schema.life_areas_schema,
         data_factory=scope.testing.fake_data.fixtures_fake_life_areas.fake_life_areas_factory(),
-        expected_valid=True,
+        expected_document=False,
+        expected_singleton=False,
+        expected_set_element=False,
+        expected_semantic_set_id=None,
     ),
     ConfigTestFakeDataSchema(
         name="patient-profile",
@@ -100,7 +133,10 @@ TEST_CONFIGS = [
         data_factory=scope.testing.fake_data.fixtures_fake_patient_profile.fake_patient_profile_factory(
             faker_factory=faker_factory,
         ),
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=True,
+        expected_set_element=False,
+        expected_semantic_set_id=None,
     ),
     ConfigTestFakeDataSchema(
         name="referral-status",
@@ -108,7 +144,10 @@ TEST_CONFIGS = [
         data_factory=scope.testing.fake_data.fixtures_fake_referral_status.fake_referral_status_factory(
             faker_factory=faker_factory,
         ),
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=False,
+        expected_set_element=False,
+        expected_semantic_set_id=None,
     ),
     ConfigTestFakeDataSchema(
         name="safety-plan",
@@ -119,7 +158,10 @@ TEST_CONFIGS = [
                 faker_factory=faker_factory,
             ),
         ),
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=True,
+        expected_set_element=False,
+        expected_semantic_set_id=None,
     ),
     ConfigTestFakeDataSchema(
         name="session",
@@ -130,7 +172,10 @@ TEST_CONFIGS = [
                 faker_factory=faker_factory,
             ),
         ),
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=False,
+        expected_set_element=True,
+        expected_semantic_set_id=scope.database.patient.sessions.SEMANTIC_SET_ID,
     ),
     ConfigTestFakeDataSchema(
         name="sessions",
@@ -143,7 +188,10 @@ TEST_CONFIGS = [
                 ),
             ),
         ),
-        expected_valid=True,
+        expected_document=False,
+        expected_singleton=False,
+        expected_set_element=False,
+        expected_semantic_set_id=None,
     ),
     ConfigTestFakeDataSchema(
         name="values-inventory",
@@ -152,9 +200,27 @@ TEST_CONFIGS = [
             faker_factory=faker_factory,
             fake_life_areas=scope.testing.fake_data.fixtures_fake_life_areas.fake_life_areas_factory()(),
         ),
-        expected_valid=True,
+        expected_document=True,
+        expected_singleton=True,
+        expected_set_element=False,
+        expected_semantic_set_id=None,
     ),
 ]
+
+
+def _assert_schema(
+    *,
+    data: Union[dict, List[dict]],
+    schema: jschon.JSONSchema,
+    expected_valid: bool,
+):
+    schema_result = schema.evaluate(jschon.JSON(data)).output("detailed")
+    if schema_result["valid"] != expected_valid:
+        pprint(data)
+        print()
+        pprint(schema_result)
+
+    assert schema_result["valid"] == expected_valid
 
 
 @pytest.mark.parametrize(
@@ -169,15 +235,79 @@ def test_fake_data_schema(config: ConfigTestFakeDataSchema):
     if config.schema is None:
         pytest.xfail("Schema failed to parse")
 
+    # Ensure valid configuration of expectations
+    if config.expected_document:
+        # May be a singleton, may be a set, or may be neither
+        # Ensure not testing both singleton and set
+        assert not (config.expected_singleton and config.expected_set_element)
+
+        # Only set elements can have a semantic set id
+        if not config.expected_set_element:
+            assert config.expected_semantic_set_id is None
+    else:
+        # Fake data is expected to be a List[dict]
+        # Assume those documents will be tested elsewhere
+        assert not config.expected_singleton
+        assert not config.expected_set_element
+        assert config.expected_semantic_set_id is None
+
     for count in range(TEST_ITERATIONS):
+        # Obtain fake data
         data = config.data_factory()
 
-        # Test the schema
-        result = config.schema.evaluate(jschon.JSON(data)).output("detailed")
-        if result["valid"] != config.expected_valid:
-            if not result["valid"]:
-                pprint(data)
-                print()
-                pprint(result)
+        # Test against the schema
+        _assert_schema(
+            data=data,
+            schema=config.schema,
+            expected_valid=True,
+        )
 
-            assert result["valid"] == config.expected_valid
+        # Fake data factories generate data with fields corresponding to
+        # data that has not been stored and retrieved from the database.
+        # For schemas that are intended for database storage,
+        # we also test they support the additional associated fields.
+
+        if config.expected_document:
+            # A document can be normalized
+            document_normalized = document_utils.normalize_document(document=data)
+
+            # Test against the schema
+            _assert_schema(
+                data=document_normalized,
+                schema=config.schema,
+                expected_valid=True,
+            )
+
+            # A document can have schema fields added or removed.
+            # It should accept or reject those fields based on expectations.
+
+            # These fields are expected by both singletons and set elements
+            # Ensure they were not already present, as that's a fake_data error
+            document_singleton = copy.deepcopy(document_normalized)
+            assert "_id" not in document_singleton
+            document_singleton["_id"] = str(bson.objectid.ObjectId())
+            assert "_rev" not in document_singleton
+            document_singleton["_rev"] = 1
+            document_singleton = document_utils.normalize_document(document=document_singleton)
+
+            _assert_schema(
+                data=document_singleton,
+                schema=config.schema,
+                expected_valid=config.expected_singleton or config.expected_set_element,
+            )
+
+            # These fields are expected by only set elements
+            # Ensure they were not already present, as that's a fake_data error
+            document_set_element = copy.deepcopy(document_singleton)
+            document_set_element["_set_id"] = collection_utils.generate_set_id()
+            assert "_set_id" not in document_singleton
+            if config.expected_semantic_set_id:
+                assert config.expected_semantic_set_id not in document_singleton
+                document_set_element[config.expected_semantic_set_id] = document_set_element["_set_id"]
+            document_set_element = document_utils.normalize_document(document=document_set_element)
+
+            _assert_schema(
+                data=document_set_element,
+                schema=config.schema,
+                expected_valid=config.expected_set_element,
+            )
