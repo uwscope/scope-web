8854f7f136ce5eb848ef0cccd4e076bbecf47bd9
James Fogarty <jayfo@users.noreply.github.com>
2022-02-24 09:00:23 -0800

    Test Profile Updates Also Maintain Identity

---
+++ scope_shared/scope/testing/test_database/test_patient_profile_maintains_identity.py
@@ -0,0 +1,54 @@
+"""
+In addition to its standard singleton properties,
+scope.database.patient.patient_profile maintains fields in the patient identity.
+"""
+
+import copy
+import pymongo.collection
+import pymongo.database
+import pytest
+from typing import Callable
+
+import scope.database.patient.patient_profile
+import scope.database.patients
+import scope.testing.fixtures_database_temp_patient
+
+
+def test_patient_profile_maintains_identity(
+    database_client: pymongo.database.Database,
+    database_temp_patient_factory: Callable[
+        [],
+        scope.testing.fixtures_database_temp_patient.DatabaseTempPatient,
+    ],
+):
+    temp_patient = database_temp_patient_factory()
+    patient_collection = temp_patient.collection
+
+    # Obtain and modify a patient profile
+    existing_profile_document = scope.database.patient.patient_profile.get_patient_profile(
+        collection=patient_collection,
+    )
+
+    assert existing_profile_document["name"] != "modified name"
+    assert existing_profile_document["MRN"] != "modified MRN"
+
+    modified_profile_document = copy.deepcopy(existing_profile_document)
+    modified_profile_document["name"] = "modified name"
+    modified_profile_document["MRN"] = "modified MRN"
+    del modified_profile_document["_id"]
+
+    scope.database.patient.patient_profile.put_patient_profile(
+        database=database_client,
+        collection=patient_collection,
+        patient_id=temp_patient.patient_id,
+        patient_profile=modified_profile_document,
+    )
+
+    # Confirm corresponding fields were also modified in patient identity
+    modified_identity = scope.database.patients.get_patient_identity(
+        database=database_client,
+        patient_id=temp_patient.patient_id,
+    )
+
+    assert modified_identity["name"] == modified_profile_document["name"]
+    assert modified_identity["MRN"] == modified_profile_document["MRN"]
