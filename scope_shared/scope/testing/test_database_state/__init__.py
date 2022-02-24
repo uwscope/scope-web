01faedfe32d3845b1071912bc9d7782d05fbbc63
James Fogarty <jayfo@users.noreply.github.com>
2022-02-24 09:52:52 -0800

    Test Database State

--- scope_shared/scope/testing/test_database_state/__init__.py
+++ scope_shared/scope/testing/test_database_state/__init__.py
@@ -4,5 +4,6 @@ Import all tests that should be executed to check database state.
 Packages can then import this single module.
 """
 
-from scope.testing.test_database_state.test_patients_collection import *
+from scope.testing.test_database_state.test_patient_identity_collection import *
+from scope.testing.test_database_state.test_provider_identity_collection import *
 from scope.testing.test_database_state.test_unique_credentials import *
