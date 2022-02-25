a7434307220eaff08684a2febf608156af696d04
James Fogarty <jayfo@users.noreply.github.com>
2022-02-25 07:32:26 -0800

    Fix Patient Cleanup in Testing

--- scope_shared/scope/testing/test_database/test_patients.py
+++ scope_shared/scope/testing/test_database/test_patients.py
@@ -20,47 +20,49 @@ def test_patient_create_get_delete(
         name="TEST NAME",
         MRN="TEST MRN",
     )
-    scope.testing.schema.assert_schema(
-        data=created_patient_identity,
-        schema=scope.schema.patient_identity_schema,
-        expected_valid=True,
-    )
 
-    # Confirm get patient identity via _set_id
-    retrieved_identity_document = scope.database.patients.get_patient_identity(
-        database=database_client,
-        patient_id=created_patient_identity["_set_id"],
-    )
-    assert retrieved_identity_document == created_patient_identity
-
-    # Confirm get patient identity via semantic set id
-    retrieved_identity_document = scope.database.patients.get_patient_identity(
-        database=database_client,
-        patient_id=created_patient_identity[
-            scope.database.patients.PATIENT_IDENTITY_SEMANTIC_SET_ID
-        ],
-    )
-    assert retrieved_identity_document == created_patient_identity
-
-    # Confirm patient identity in list of patient identities
-    patients = scope.database.patients.get_patient_identities(database=database_client)
-    assert created_patient_identity in patients
-
-    # Confirm patient profile document now exists
-    retrieved_profile = scope.database.patient.patient_profile.get_patient_profile(
-        collection=database_client.get_collection(
-            created_patient_identity["collection"]
+    try:
+        scope.testing.schema.assert_schema(
+            data=created_patient_identity,
+            schema=scope.schema.patient_identity_schema,
+            expected_valid=True,
         )
-    )
-    assert retrieved_profile is not None
 
-    # Delete patient
-    result = scope.database.patients.delete_patient(
-        database=database_client,
-        patient_id=created_patient_identity["_set_id"],
-        destructive=True,
-    )
-    assert result
+        # Confirm get patient identity via _set_id
+        retrieved_identity_document = scope.database.patients.get_patient_identity(
+            database=database_client,
+            patient_id=created_patient_identity["_set_id"],
+        )
+        assert retrieved_identity_document == created_patient_identity
+
+        # Confirm get patient identity via semantic set id
+        retrieved_identity_document = scope.database.patients.get_patient_identity(
+            database=database_client,
+            patient_id=created_patient_identity[
+                scope.database.patients.PATIENT_IDENTITY_SEMANTIC_SET_ID
+            ],
+        )
+        assert retrieved_identity_document == created_patient_identity
+
+        # Confirm patient identity in list of patient identities
+        patients = scope.database.patients.get_patient_identities(database=database_client)
+        assert created_patient_identity in patients
+
+        # Confirm patient profile document now exists
+        retrieved_profile = scope.database.patient.patient_profile.get_patient_profile(
+            collection=database_client.get_collection(
+                created_patient_identity["collection"]
+            )
+        )
+        assert retrieved_profile is not None
+    finally:
+        # Delete patient
+        result = scope.database.patients.delete_patient(
+            database=database_client,
+            patient_id=created_patient_identity["_set_id"],
+            destructive=True,
+        )
+        assert result
 
     # Confirm get patient identity now fails
     retrieved_identity_document = scope.database.patients.get_patient_identity(
@@ -104,23 +106,31 @@ def test_patient_identity_update(
         name="TEST NAME",
         MRN="TEST MRN",
     )
-    scope.testing.schema.assert_schema(
-        data=created_patient_identity,
-        schema=scope.schema.patient_identity_schema,
-        expected_valid=True,
-    )
 
-    # Modify the identity
-    modified_patient_identity = copy.deepcopy(created_patient_identity)
-    modified_patient_identity["name"] = "MODIFIED NAME"
-    modified_patient_identity["MRN"] = "MODIFIED MRN"
-    del modified_patient_identity["_id"]
+    try:
+        scope.testing.schema.assert_schema(
+            data=created_patient_identity,
+            schema=scope.schema.patient_identity_schema,
+            expected_valid=True,
+        )
 
-    result = scope.database.patients.put_patient_identity(
-        database=database_client,
-        patient_id=created_patient_identity["_set_id"],
-        patient_identity=modified_patient_identity,
-    )
+        # Modify the identity
+        modified_patient_identity = copy.deepcopy(created_patient_identity)
+        modified_patient_identity["name"] = "MODIFIED NAME"
+        modified_patient_identity["MRN"] = "MODIFIED MRN"
+        del modified_patient_identity["_id"]
 
-    assert result.inserted_count == 1
-    assert result.document["_rev"] == 2
+        result = scope.database.patients.put_patient_identity(
+            database=database_client,
+            patient_id=created_patient_identity["_set_id"],
+            patient_identity=modified_patient_identity,
+        )
+
+        assert result.inserted_count == 1
+        assert result.document["_rev"] == 2
+    finally:
+        scope.database.patients.delete_patient(
+            database=database_client,
+            patient_id=created_patient_identity["patientId"],
+            destructive=True
+        )
