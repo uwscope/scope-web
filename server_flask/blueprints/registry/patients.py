2e4d7bc37b132931b57cadb956bbaad48dfdb3fa
Anant Mittal <anmittal@cs.washington.edu>
2022-02-28 22:00:50 -0800

    Added all sets and singletons to construct patient document

--- server_flask/blueprints/registry/patients.py
+++ server_flask/blueprints/registry/patients.py
@@ -5,10 +5,17 @@ import pymongo.collection
 
 import request_utils
 from request_context import request_context
+import scope.database.patient.activity_logs
+import scope.database.patient.activities
+import scope.database.patient.assessment_logs
+import scope.database.patient.assessments
 import scope.database.patient.case_reviews
 import scope.database.patient.clinical_history
+import scope.database.patient.mood_logs
 import scope.database.patient.patient_profile
 import scope.database.patient.safety_plan
+import scope.database.patient.scheduled_activities
+import scope.database.patient.scheduled_assessments
 import scope.database.patient.sessions
 import scope.database.patient.values_inventory
 import scope.database.patients
@@ -38,19 +45,19 @@ def _construct_patient_document(
     if profile:
         patient_document["profile"] = profile
 
-    # # Case reviews
-    # case_reviews = scope.database.patient.case_reviews.get_case_reviews(
-    #     collection=patient_collection
-    # )
-    # if case_reviews:
-    #     patient_document["caseReviews"] = case_reviews
-
     # Clinical history
     clinical_history = scope.database.patient.clinical_history.get_clinical_history(
         collection=patient_collection
     )
     if clinical_history:
         patient_document["clinicalHistory"] = clinical_history
+
+    # Values inventory
+    values_inventory = scope.database.patient.values_inventory.get_values_inventory(
+        collection=patient_collection
+    )
+    if values_inventory:
+        patient_document["valuesInventory"] = values_inventory
 
     # Safety plan
     safety_plan = scope.database.patient.safety_plan.get_safety_plan(
@@ -66,12 +73,65 @@ def _construct_patient_document(
     if sessions:
         patient_document["sessions"] = sessions
 
-    # Values inventory
-    values_inventory = scope.database.patient.values_inventory.get_values_inventory(
+    # Case reviews
+    case_reviews = scope.database.patient.case_reviews.get_case_reviews(
         collection=patient_collection
     )
-    if values_inventory:
-        patient_document["valuesInventory"] = values_inventory
+    if case_reviews:
+        patient_document["caseReviews"] = case_reviews
+
+    # Assessments
+    assessments = scope.database.patient.assessments.get_assessments(
+        collection=patient_collection
+    )
+    if assessments:
+        patient_document["assessments"] = assessments
+
+    # Scheduled Assessments
+    scheduled_assessments = (
+        scope.database.patient.scheduled_assessments.get_scheduled_assessments(
+            collection=patient_collection
+        )
+    )
+    if scheduled_assessments:
+        patient_document["scheduledAssessments"] = scheduled_assessments
+
+    # Assessment Logs
+    assessment_logs = scope.database.patient.assessment_logs.get_assessment_logs(
+        collection=patient_collection
+    )
+    if assessment_logs:
+        patient_document["assessmentLogs"] = assessment_logs
+
+    # Activities
+    activities = scope.database.patient.activities.get_activities(
+        collection=patient_collection
+    )
+    if activities:
+        patient_document["activities"] = activities
+
+    # Schedule activities
+    scheduled_activities = (
+        scope.database.patient.scheduled_activities.get_scheduled_activities(
+            collection=patient_collection
+        )
+    )
+    if scheduled_activities:
+        patient_document["scheduledActivities"] = scheduled_activities
+
+    # Activity Logs
+    activity_logs = scope.database.patient.activity_logs.get_activity_logs(
+        collection=patient_collection
+    )
+    if activity_logs:
+        patient_document["activityLogs"] = activity_logs
+
+    # Mood logs
+    mood_logs = scope.database.patient.mood_logs.get_mood_logs(
+        collection=patient_collection
+    )
+    if mood_logs:
+        patient_document["moodLogs"] = mood_logs
 
     return patient_document
 
