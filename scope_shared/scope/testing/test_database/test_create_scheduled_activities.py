055944b563b454b0faf18ca884ce41a2b9a65e79
Anant Mittal <anmittal@cs.washington.edu>
2022-03-02 11:57:30 -0800

    Refactored test_create_scheduled_activities

--- scope_shared/scope/testing/test_database/test_create_scheduled_activities.py
+++ scope_shared/scope/testing/test_database/test_create_scheduled_activities.py
@@ -9,7 +9,26 @@ import scope.schema
 import scope.schema_utils as schema_utils
 
 
-def _create_scheduled_activities_assertions(scheduled_activities: List[dict]) -> None:
+def _dummy_inserted_activity(
+    fake_activity_factory: Callable[[], dict],
+    has_repetition: bool,
+    has_reminder: bool = False,
+) -> dict:
+    activity = fake_activity_factory()
+    # Assume activity has just been inserted into db using POST
+    # create_scheduled_activities method needs activity to have its SEMANTIC_SET_ID
+    activity[
+        scope.database.patient.activities.SEMANTIC_SET_ID
+    ] = collection_utils.generate_set_id()
+    activity["hasRepetition"] = has_repetition
+    activity["hasReminder"] = has_reminder
+    return activity
+
+
+def _scheduled_activities_assertions(
+    activity: dict,
+    scheduled_activities: List[dict],
+) -> None:
     schema_utils.assert_schema(
         data=scheduled_activities,
         schema=scope.schema.scheduled_activities_schema,
@@ -18,67 +37,89 @@ def _create_scheduled_activities_assertions(scheduled_activities: List[dict]) ->
         assert not scheduled_activity_current["completed"]
         assert scheduled_activity_current["dueType"] == "Exact"
 
+    if not activity["hasRepetition"]:
+        assert len(scheduled_activities) == 1
+        assert scheduled_activities[0]["dueDate"] == date_utils.format_datetime(
+            datetime.datetime.combine(
+                date_utils.parse_date(activity["startDate"]),
+                datetime.time(hour=activity["timeOfDay"]),
+            )
+        )
+        if not activity["hasReminder"]:
+            assert scheduled_activities[0]["reminder"] == None
+        else:
+            assert scheduled_activities[0]["reminder"] == date_utils.format_datetime(
+                datetime.datetime.combine(
+                    date_utils.parse_date(activity["startDate"]),
+                    datetime.time(hour=activity["reminderTimeOfDay"]),
+                )
+            )
 
-# TODO: Discuss w/ James if this can parametrized.
-def test_create_scheduled_activities(
+
+# TODO: Check if "test" function names could be better.
+def test_create_scheduled_activities_condition_one(
     data_fake_activity_factory: Callable[[], dict],
 ):
-    activity = data_fake_activity_factory()
-    # Assume activity has just been inserted into db using POST
-    # create_scheduled_activities method needs activity to have its SEMANTIC_SET_ID
-    activity[
-        scope.database.patient.activities.SEMANTIC_SET_ID
-    ] = collection_utils.generate_set_id()
+    # Condition 1 - hasRepetition is False
+    activity = _dummy_inserted_activity(
+        fake_activity_factory=data_fake_activity_factory,
+        has_repetition=False,
+    )
 
-    # OPTION 1 - hasRepetition is False
-    activity["hasRepetition"] = False
-    start_date = datetime.date.today()
-    time_of_day = 8
-    activity["startDate"] = date_utils.format_date(start_date)
-    activity["timeOfDay"] = time_of_day
     scheduled_activities = (
         scope.database.patient.scheduled_activities.create_scheduled_activities(
             activity=activity
         )
     )
-    assert len(scheduled_activities) == 1
-    assert scheduled_activities[0]["dueDate"] == date_utils.format_datetime(
-        datetime.datetime.combine(start_date, datetime.time(hour=time_of_day))
-    )
-    _create_scheduled_activities_assertions(scheduled_activities=scheduled_activities)
 
-    # OPTION 2 - hasRepetition is False and hasReminder is False
-    activity["hasRepetition"] = False
-    activity["hasReminder"] = False
+    _scheduled_activities_assertions(
+        activity=activity,
+        scheduled_activities=scheduled_activities,
+    )
+
+
+def test_create_scheduled_activities_condition_one_subcondition_one(
+    data_fake_activity_factory: Callable[[], dict],
+):
+    # Condition 1 - hasRepetition is False, Subcondition 1 - hasReminder is False
+    activity = _dummy_inserted_activity(
+        fake_activity_factory=data_fake_activity_factory,
+        has_repetition=False,
+        has_reminder=False,
+    )
+
     scheduled_activities = (
         scope.database.patient.scheduled_activities.create_scheduled_activities(
             activity=activity
         )
     )
-    assert len(scheduled_activities) == 1
-    assert scheduled_activities[0]["dueDate"] == date_utils.format_datetime(
-        datetime.datetime.combine(start_date, datetime.time(hour=time_of_day))
-    )
-    assert scheduled_activities[0]["reminder"] == None
-    _create_scheduled_activities_assertions(scheduled_activities=scheduled_activities)
 
-    # OPTION 3 - hasRepetition is False and hasReminder is True
-    activity["hasRepetition"] = False
-    activity["hasReminder"] = True
-    reminder_time_of_day = 8
-    activity["reminderTimeOfDay"] = reminder_time_of_day
+    _scheduled_activities_assertions(
+        activity=activity,
+        scheduled_activities=scheduled_activities,
+    )
+
+
+def test_create_scheduled_activities_condition_one_subcondition_two(
+    data_fake_activity_factory: Callable[[], dict],
+):
+    # Condition 1 - hasRepetition is False, Subcondition 2 - hasReminder is True
+
+    activity = _dummy_inserted_activity(
+        fake_activity_factory=data_fake_activity_factory,
+        has_repetition=False,
+        has_reminder=True,
+    )
+
     scheduled_activities = (
         scope.database.patient.scheduled_activities.create_scheduled_activities(
             activity=activity
         )
     )
-    assert len(scheduled_activities) == 1
-    assert scheduled_activities[0]["dueDate"] == date_utils.format_datetime(
-        datetime.datetime.combine(start_date, datetime.time(hour=time_of_day))
+
+    _scheduled_activities_assertions(
+        activity=activity,
+        scheduled_activities=scheduled_activities,
     )
-    assert scheduled_activities[0]["reminder"] == date_utils.format_datetime(
-        datetime.datetime.combine(start_date, datetime.time(hour=reminder_time_of_day))
-    )
-    _create_scheduled_activities_assertions(scheduled_activities=scheduled_activities)
 
     # TODO: hasRepetition is True left.
