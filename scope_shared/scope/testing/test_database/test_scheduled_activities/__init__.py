c45ea86e1812d0b6fc2a60a77428c787e4438828
Anant Mittal <anmittal@cs.washington.edu>
2022-03-06 15:49:08 -0800

    Updated __init__.py

--- scope_shared/scope/testing/test_database/test_scheduled_activities/__init__.py
+++ scope_shared/scope/testing/test_database/test_scheduled_activities/__init__.py
@@ -3,4 +3,4 @@ Module testing scheduled activities.
 """
 
 from scope.testing.test_database.test_scheduled_activities.test_create_scheduled_activities import *
-from scope.testing.test_database.test_scheduled_activities.test_get_filtered_scheduled_activities import *
+from scope.testing.test_database.test_scheduled_activities.test_filter_scheduled_activities import *
