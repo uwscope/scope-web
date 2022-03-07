12d31a3ba059bef634846b4e2f1d3bf11c5b9a2b
James Fogarty <jayfo@users.noreply.github.com>
2022-03-07 07:18:09 -0800

    Remove Assessment Post

--- server_flask/blueprints/registry/assessments.py
+++ server_flask/blueprints/registry/assessments.py
@@ -38,49 +38,6 @@ def get_assessments(patient_id):
     }
 
 
-# TODO: @James, commented out POST.
-"""
-@assessments_blueprint.route(
-    "/<string:patient_id>/assessments",
-    methods=["POST"],
-)
-@request_utils.validate_schema(
-    schema=assessment_schema,
-    key="assessment",
-)
-@flask_json.as_json
-def post_assessments(patient_id):
-    # TODO: Require authentication
-
-    context = request_context()
-    patient_collection = context.patient_collection(patient_id=patient_id)
-
-    # Obtain the document being put
-    document = flask.request.json["assessment"]
-
-    # Validate and normalize the request
-    document = request_utils.set_post_request_validate(
-        semantic_set_id=scope.database.patient.assessments.SEMANTIC_SET_ID,
-        document=document,
-    )
-
-    # Store the document
-    result = scope.database.patient.assessments.post_assessment(
-        collection=patient_collection,
-        assessment=document,
-    )
-
-    # Validate and normalize the response
-    document_response = request_utils.set_post_response_validate(
-        document=result.document,
-    )
-
-    return {
-        "assessment": document_response,
-    }
-"""
-
-
 @assessments_blueprint.route(
     "/<string:patient_id>/assessment/<string:assessment_id>",
     methods=["GET"],
