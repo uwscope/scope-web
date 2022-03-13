0b8d0e0ec0740fa420de6c56ac569628ef16894b
Anant Mittal <anmittal@cs.washington.edu>
2022-03-12 18:30:29 -0800

    Updated scheduled_activities db layer

--- scope_shared/scope/database/patient/scheduled_activities.py
+++ scope_shared/scope/database/patient/scheduled_activities.py
@@ -1,3 +1,4 @@
+import copy
 import datetime
 import dateutil.rrule
 import dateutil.parser
@@ -15,191 +16,182 @@ DOCUMENT_TYPE = "scheduledActivity"
 SEMANTIC_SET_ID = "scheduledActivityId"
 
 
-def _compute_byweekday(repeat_day_flags: dict) -> Tuple:
-    byweekday = []
-    for day, day_flag in repeat_day_flags.items():
-        if day_flag:
-            byweekday.append(date_utils.DATEUTIL_WEEKDAYS_MAP[day])
-
-    return byweekday
-
-
-def _compute_scheduled_activity_properties(
-    activity: dict, scheduled_activity_due_date: str
-) -> dict:
-    scheduled_activity = {
-        "_type": DOCUMENT_TYPE,
-        "dueType": "Exact",
-        "activityId": activity[scope.database.patient.activities.SEMANTIC_SET_ID],
-        "activityName": activity["name"],
-        "timeOfDay": activity["timeOfDay"],
-        "completed": False,
-    }
-    scheduled_activity["dueDate"] = date_utils.format_datetime(
-        date_utils.parse_date(scheduled_activity_due_date)
-        # + datetime.timedelta(hours=activity["timeOfDay"]) # TODO: Comment this until we fix the timezone concern.
-    )
-    if activity.get("hasReminder"):
-        scheduled_activity["reminderTimeOfDay"] = activity["reminderTimeOfDay"]
-        # TODO: Comment this until we fix the timezone concern.
-        # scheduled_activity["reminder"] = date_utils.format_datetime(
-        #     date_utils.parse_date(scheduled_activity_due_date)
-        #     + datetime.timedelta(hours=activity["reminderTimeOfDay"])
-        # )
-    return scheduled_activity
+# TODO: Anant; Keep the commented code for now.
+# def _compute_scheduled_activity_properties(
+#     activity: dict, scheduled_activity_due_date: str
+# ) -> dict:
+#     scheduled_activity = {
+#         "_type": DOCUMENT_TYPE,
+#         # "dueType": "Exact",
+#         "activityId": activity[scope.database.patient.activities.SEMANTIC_SET_ID],
+#         "activityName": activity["name"],
+#         "timeOfDay": activity["timeOfDay"],
+#         "completed": False,
+#     }
+#     scheduled_activity["dueDate"] = date_utils.format_datetime(
+#         date_utils.parse_date(scheduled_activity_due_date)
+#         # + datetime.timedelta(hours=activity["timeOfDay"])
+#     )
+#     if activity.get("hasReminder"):
+#         scheduled_activity["reminderTimeOfDay"] = activity["reminderTimeOfDay"]
+#
+#         # scheduled_activity["reminder"] = date_utils.format_datetime(
+#         #     date_utils.parse_date(scheduled_activity_due_date)
+#         #     + datetime.timedelta(hours=activity["reminderTimeOfDay"])
+#         # )
+#     return scheduled_activity
 
 
-def create_scheduled_activities(
-    *,
-    activity: dict,
-    weeks: int = 12,
-    activity_method: str,
-) -> List[dict]:
+# def create_scheduled_activities(
+#     *,
+#     activity: dict,
+#     weeks: int = 12,
+#     activity_method: str,
+# ) -> List[dict]:
 
-    if activity_method == "put":
-        start_date = date_utils.parse_date(activity["startDate"])
-        todays_date = date_utils.parse_date(
-            date_utils.format_date(datetime.date.today())
-        )
-        if start_date < todays_date:
-            activity.update(
-                {"startDate": date_utils.format_date(datetime.date.today())}
-            )
+#     if activity_method == "put":
+#         start_date = date_utils.parse_date(activity["startDate"])
+#         todays_date = date_utils.parse_date(
+#             date_utils.format_date(datetime.date.today())
+#         )
+#         if start_date < todays_date:
+#             activity.update(
+#                 {"startDate": date_utils.format_date(datetime.date.today())}
+#             )
 
-    scheduled_activities = []
-    if not activity.get("hasRepetition"):
-        # Create 1 scheduled activity
-        scheduled_activity = _compute_scheduled_activity_properties(
-            activity=activity, scheduled_activity_due_date=activity["startDate"]
-        )
-        scheduled_activities.append(scheduled_activity)
-    else:
-        # Create future dates using startDate and repeatDayFlags for the next 3 months
-        scheduled_activity_due_dates = list(
-            dateutil.rrule.rrule(
-                dateutil.rrule.WEEKLY,
-                dtstart=date_utils.parse_date(activity["startDate"]),
-                until=date_utils.parse_date(activity["startDate"])
-                + datetime.timedelta(weeks=weeks),
-                byweekday=_compute_byweekday(
-                    repeat_day_flags=activity["repeatDayFlags"]
-                ),
-            )
-        )
-        for scheduled_activity_due_date_current in scheduled_activity_due_dates:
-            scheduled_activity = _compute_scheduled_activity_properties(
-                activity=activity,
-                scheduled_activity_due_date=date_utils.format_date(
-                    scheduled_activity_due_date_current
-                ),
-            )
-            scheduled_activities.append(scheduled_activity)
+#     scheduled_activities = []
+#     if not activity.get("hasRepetition"):
+#         # Create 1 scheduled activity
+#         scheduled_activity = _compute_scheduled_activity_properties(
+#             activity=activity, scheduled_activity_due_date=activity["startDate"]
+#         )
+#         scheduled_activities.append(scheduled_activity)
+#     else:
+#         # Create future dates using startDate and repeatDayFlags for the next 3 months
+#         scheduled_activity_due_dates = list(
+#             dateutil.rrule.rrule(
+#                 dateutil.rrule.WEEKLY,
+#                 dtstart=date_utils.parse_date(activity["startDate"]),
+#                 until=date_utils.parse_date(activity["startDate"])
+#                 + datetime.timedelta(weeks=weeks),
+#                 byweekday=_compute_byweekday(
+#                     repeat_day_flags=activity["repeatDayFlags"]
+#                 ),
+#             )
+#         )
+#         for scheduled_activity_due_date_current in scheduled_activity_due_dates:
+#             scheduled_activity = _compute_scheduled_activity_properties(
+#                 activity=activity,
+#                 scheduled_activity_due_date=date_utils.format_date(
+#                     scheduled_activity_due_date_current
+#                 ),
+#             )
+#             scheduled_activities.append(scheduled_activity)
 
-    return scheduled_activities
-
-
-def create_and_post_scheduled_activities(
-    *,
-    collection: pymongo.collection.Collection,
-    activity: dict,
-    weeks: int = 12,  # ~ 3 months
-    activity_method: str,
-):
-    # Create scheduledActivities here
-    scheduled_activities = create_scheduled_activities(
-        activity=activity,
-        weeks=weeks,
-        activity_method=activity_method,
-    )
-
-    schema_utils.raise_for_invalid_schema(
-        data=scheduled_activities,
-        schema=scope.schema.scheduled_activities_schema,
-    )
-
-    for scheduled_activity_current in scheduled_activities:
-
-        post_scheduled_activity(
-            collection=collection,
-            scheduled_activity=scheduled_activity_current,
-        )
+#     return scheduled_activities
 
 
-# TODO: Can this method be better named.
-def filter_scheduled_activities(
-    *,
-    all_scheduled_activities: List[dict],
-    activity: dict,
-) -> List[dict]:
-    """
-    GET future due date scheduled activities for an activity
-    """
+# def create_and_post_scheduled_activities(
+#     *,
+#     collection: pymongo.collection.Collection,
+#     activity: dict,
+#     weeks: int = 12,  # ~ 3 months
+#     activity_method: str,
+# ):
+#     # Create scheduledActivities here
+#     scheduled_activities = create_scheduled_activities(
+#         activity=activity,
+#         weeks=weeks,
+#         activity_method=activity_method,
+#     )
 
-    if all_scheduled_activities:
-        filtered_scheduled_activities = list(
-            filter(
-                lambda all_scheduled_activity_current: (
-                    # Keep scheduled activities which match activity
-                    (
-                        all_scheduled_activity_current[
-                            scope.database.patient.activities.SEMANTIC_SET_ID
-                        ]
-                        == activity[scope.database.patient.activities.SEMANTIC_SET_ID]
-                    )
-                    # Keep scheduled activities which are due in future
-                    and (
-                        date_utils.parse_date(all_scheduled_activity_current["dueDate"])
-                        > date_utils.parse_date(
-                            date_utils.format_date(datetime.date.today())
-                        )
-                    )
-                ),
-                all_scheduled_activities,
-            )
-        )
-        return filtered_scheduled_activities
-    return []
+#     schema_utils.raise_for_invalid_schema(
+#         data=scheduled_activities,
+#         schema=scope.schema.scheduled_activities_schema,
+#     )
+
+#     for scheduled_activity_current in scheduled_activities:
+
+#         post_scheduled_activity(
+#             collection=collection,
+#             scheduled_activity=scheduled_activity_current,
+#         )
 
 
-def get_filter_and_put_scheduled_activities(
-    *,
-    collection: pymongo.collection.Collection,
-    activity: dict,
-):
+# def filter_scheduled_activities(
+#     *,
+#     all_scheduled_activities: List[dict],
+#     activity: dict,
+# ) -> List[dict]:
+#     """
+#     GET future due date scheduled activities for an activity
+#     """
 
-    # GET all stored scheduledActivities
-    all_scheduled_activities = get_scheduled_activities(
-        collection=collection,
-    )
+#     if all_scheduled_activities:
+#         filtered_scheduled_activities = list(
+#             filter(
+#                 lambda all_scheduled_activity_current: (
+#                     # Keep scheduled activities which match activity
+#                     (
+#                         all_scheduled_activity_current[
+#                             scope.database.patient.activities.SEMANTIC_SET_ID
+#                         ]
+#                         == activity[scope.database.patient.activities.SEMANTIC_SET_ID]
+#                     )
+#                     # Keep scheduled activities which are due in future
+#                     and (
+#                         date_utils.parse_date(all_scheduled_activity_current["dueDate"])
+#                         > date_utils.parse_date(
+#                             date_utils.format_date(datetime.date.today())
+#                         )
+#                     )
+#                 ),
+#                 all_scheduled_activities,
+#             )
+#         )
+#         return filtered_scheduled_activities
+#     return []
 
-    # Get future due date scheduled activities for activity
-    filtered_scheduled_activities = filter_scheduled_activities(
-        all_scheduled_activities=all_scheduled_activities,
-        activity=activity,
-    )
 
-    schema_utils.raise_for_invalid_schema(
-        data=filtered_scheduled_activities,
-        schema=scope.schema.scheduled_activities_schema,
-    )
+# def get_filter_and_put_scheduled_activities(
+#     *,
+#     collection: pymongo.collection.Collection,
+#     activity: dict,
+# ):
 
-    # PUT the filtered scheduled activities
-    for scheduled_activity_current in filtered_scheduled_activities:
-        # Delete _id for putting
-        del scheduled_activity_current["_id"]
-        # Mark them as deleted
-        scheduled_activity_current.update({"_deleted": True})
+#     # GET all stored scheduledActivities
+#     all_scheduled_activities = get_scheduled_activities(
+#         collection=collection,
+#     )
 
-        schema_utils.raise_for_invalid_schema(
-            data=scheduled_activity_current,
-            schema=scope.schema.scheduled_activity_schema,
-        )
+#     # Get future due date scheduled activities for activity
+#     filtered_scheduled_activities = filter_scheduled_activities(
+#         all_scheduled_activities=all_scheduled_activities,
+#         activity=activity,
+#     )
 
-        put_scheduled_activity(
-            collection=collection,
-            scheduled_activity=scheduled_activity_current,
-            set_id=scheduled_activity_current["_set_id"],
-        )
+#     schema_utils.raise_for_invalid_schema(
+#         data=filtered_scheduled_activities,
+#         schema=scope.schema.scheduled_activities_schema,
+#     )
+
+#     # PUT the filtered scheduled activities
+#     for scheduled_activity_current in filtered_scheduled_activities:
+#         # Delete _id for putting
+#         del scheduled_activity_current["_id"]
+#         # Mark them as deleted
+#         scheduled_activity_current.update({"_deleted": True})
+
+#         schema_utils.raise_for_invalid_schema(
+#             data=scheduled_activity_current,
+#             schema=scope.schema.scheduled_activity_schema,
+#         )
+
+#         put_scheduled_activity(
+#             collection=collection,
+#             scheduled_activity=scheduled_activity_current,
+#             set_id=scheduled_activity_current["_set_id"],
+#         )
 
 
 def get_scheduled_activities(
@@ -213,6 +205,29 @@ def get_scheduled_activities(
     return scope.database.collection_utils.get_set(
         collection=collection,
         document_type=DOCUMENT_TYPE,
+    )
+
+
+def delete_scheduled_activity(
+    *,
+    collection: pymongo.collection.Collection,
+    scheduled_activity: dict,
+    set_id: str,
+) -> scope.database.collection_utils.SetPutResult:
+    scheduled_activity = copy.deepcopy(scheduled_activity)
+
+    scheduled_activity["_deleted"] = True
+    del scheduled_activity["_id"]
+
+    schema_utils.assert_schema(
+        data=scheduled_activity,
+        schema=scope.schema.scheduled_activity_schema,
+    )
+
+    return put_scheduled_activity(
+        collection=collection,
+        scheduled_activity=scheduled_activity,
+        set_id=set_id,
     )
 
 
