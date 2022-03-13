9c0b0d5d8fe36727b4c587fa670d98315467be5d
James Fogarty <jayfo@users.noreply.github.com>
2022-03-13 11:02:05 -0700

    DRY on Parameter Check

--- scope_shared/scope/database/scheduled_item_utils.py
+++ scope_shared/scope/database/scheduled_item_utils.py
@@ -213,8 +213,6 @@ def create_scheduled_items(
             raise ValueError("reminder_time_of_day must be int >=0 and <= 23")
     if timezone.zone not in pytz.all_timezones:
         raise ValueError("timezone must be valid pytz timezone")
-    if repeat_day_flags and day_of_week:
-        raise ValueError("both repeat_day_flags and day_of_week cannot be True")
 
     scheduled_dates = _scheduled_dates(
         has_repetition=has_repetition,
