81f2f5693c3b3a71d69bfc83ed4cd81b432c2d94
Anant Mittal <anmittal@cs.washington.edu>
2022-03-31 20:23:26 -0700

    Updated GetFormDialog.tsx

--- web_patient/src/components/Forms/GetFormDialog.tsx
+++ web_patient/src/components/Forms/GetFormDialog.tsx
@@ -2,20 +2,22 @@ import React, { FunctionComponent } from 'react';
 import { ActivityLoggingForm } from 'src/components/Forms/ActivityLoggingForm';
 import AddEditActivityForm from 'src/components/Forms/AddEditActivityForm';
 import { AssessmentForm } from 'src/components/Forms/AssessmentForm';
+import MedicationLoggingForm from 'src/components/Forms/MedicationLoggingForm';
 import MoodLoggingForm from 'src/components/Forms/MoodLoggingForm';
 import SafetyPlanForm from 'src/components/Forms/SafetyPlanForm';
 import { getRouteParameter, Parameters, ParameterValues } from 'src/services/routes';
 
 const formComponents: { [paramName: string]: FunctionComponent<IFormProps> } = {
-    [ParameterValues.form.moodLog]: MoodLoggingForm,
-    [ParameterValues.form.assessmentLog]: AssessmentForm,
     [ParameterValues.form.activityLog]: ActivityLoggingForm,
     [ParameterValues.form.addActivity]: AddEditActivityForm,
+    [ParameterValues.form.assessmentLog]: AssessmentForm,
     [ParameterValues.form.editActivity]: AddEditActivityForm,
+    [ParameterValues.form.medicationLog]: MedicationLoggingForm,
+    [ParameterValues.form.moodLog]: MoodLoggingForm,
     [ParameterValues.form.safetyPlan]: SafetyPlanForm,
 };
 
-export interface IFormProps {}
+export interface IFormProps { }
 
 export default () => {
     const formName = getRouteParameter(Parameters.form);
