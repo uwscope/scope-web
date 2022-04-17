ed47c24e494af586c02b3646846260870af9319a
Jina Suh <jasmart@gmail.com>
2022-04-17 15:16:48 -0700

    Fix activity tracking #288 #287

--- web_patient/src/components/Progress/ActivityTrackingHome.tsx
+++ web_patient/src/components/Progress/ActivityTrackingHome.tsx
@@ -21,25 +21,46 @@ export const ClickableTableRow = styled(TableRow)({
 
 export const ActivityTrackingHome: FunctionComponent = observer(() => {
     const navigate = useNavigate();
-    const { patientStore } = useStores();
+    const rootStore = useStores();
+    const { patientStore } = rootStore;
 
-    const viewState = useLocalObservable<{ selectedLog?: IActivityLog; isOpen: boolean }>(() => ({
+    const viewState = useLocalObservable<{
+        selectedLog?: IActivityLog;
+        isOpen: boolean;
+        selectedLifearea?: string;
+        selectedValue?: string;
+    }>(() => ({
         selectedLog: undefined,
         isOpen: false,
+        selectedLifearea: undefined,
+        selectedValue: undefined,
     }));
 
     const handleGoBack = action(() => {
         navigate(-1);
     });
 
-    const getSuccessString = (success?: ActivitySuccessType) => {
+    const getSuccessStringShort = (success?: ActivitySuccessType) => {
         switch (success) {
             case 'Yes':
                 return getString('Activity_tracking_success_yes');
             case 'SomethingElse':
-                return getString('Activity_tracking_success_no');
-            case 'No':
                 return getString('Activity_tracking_success_alt');
+            case 'No':
+                return getString('Activity_tracking_success_no');
+            default:
+                return getString('Activity_tracking_success_none');
+        }
+    };
+
+    const getSuccessString = (success?: ActivitySuccessType) => {
+        switch (success) {
+            case 'Yes':
+                return getString('Form_activity_log_success_yes');
+            case 'SomethingElse':
+                return getString('Form_activity_log_success_something_else');
+            case 'No':
+                return getString('Form_activity_log_success_no');
             default:
                 return getString('Activity_tracking_success_none');
         }
@@ -48,11 +69,18 @@ export const ActivityTrackingHome: FunctionComponent = observer(() => {
     const handleLogClick = action((log: IActivityLog) => {
         viewState.selectedLog = log;
         viewState.isOpen = true;
+        const activity = patientStore.getActivityById(log.activityId);
+        viewState.selectedValue = activity?.value || getString('Activity_tracking_log_value_none');
+
+        const lifearea = activity && rootStore.getLifeAreaContent(activity?.lifeareaId);
+        viewState.selectedLifearea = lifearea?.name || getString('Activity_tracking_log_lifearea_none');
     });
 
     const handleClose = action(() => {
         viewState.selectedLog = undefined;
         viewState.isOpen = false;
+        viewState.selectedLifearea = undefined;
+        viewState.selectedValue = undefined;
     });
 
     return (
@@ -78,7 +106,7 @@ export const ActivityTrackingHome: FunctionComponent = observer(() => {
                                             {`${format(log.recordedDateTime, 'MM/dd')}`}
                                         </TableCell>
                                         <TableCell>{log.activityName}</TableCell>
-                                        <TableCell>{getSuccessString(log.success)}</TableCell>
+                                        <TableCell>{getSuccessStringShort(log.success)}</TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
@@ -106,15 +134,13 @@ export const ActivityTrackingHome: FunctionComponent = observer(() => {
                                                 <TableCell component="th" scope="row">
                                                     {getString('Activity_tracking_column_lifearea')}
                                                 </TableCell>
-                                                <TableCell>
-                                                    {getString('Activity_tracking_log_lifearea_none')}
-                                                </TableCell>
+                                                <TableCell>{viewState.selectedLifearea}</TableCell>
                                             </TableRow>
                                             <TableRow>
                                                 <TableCell component="th" scope="row">
                                                     {getString('Activity_tracking_column_value')}
                                                 </TableCell>
-                                                <TableCell>{getString('Activity_tracking_log_value_none')}</TableCell>
+                                                <TableCell>{viewState.selectedValue}</TableCell>
                                             </TableRow>
                                             <TableRow>
                                                 <TableCell component="th" scope="row">
@@ -125,18 +151,22 @@ export const ActivityTrackingHome: FunctionComponent = observer(() => {
                                                         getSuccessString(viewState.selectedLog.success)}
                                                 </TableCell>
                                             </TableRow>
-                                            <TableRow>
-                                                <TableCell component="th" scope="row">
-                                                    {getString('Activity_tracking_column_pleasure')}
-                                                </TableCell>
-                                                <TableCell>{viewState.selectedLog?.pleasure}</TableCell>
-                                            </TableRow>
-                                            <TableRow>
-                                                <TableCell component="th" scope="row">
-                                                    {getString('Activity_tracking_column_accomplishment')}
-                                                </TableCell>
-                                                <TableCell>{viewState.selectedLog?.accomplishment}</TableCell>
-                                            </TableRow>
+                                            {viewState.selectedLog.success != 'No' && (
+                                                <TableRow>
+                                                    <TableCell component="th" scope="row">
+                                                        {getString('Activity_tracking_column_pleasure')}
+                                                    </TableCell>
+                                                    <TableCell>{viewState.selectedLog?.pleasure}</TableCell>
+                                                </TableRow>
+                                            )}
+                                            {viewState.selectedLog.success != 'No' && (
+                                                <TableRow>
+                                                    <TableCell component="th" scope="row">
+                                                        {getString('Activity_tracking_column_accomplishment')}
+                                                    </TableCell>
+                                                    <TableCell>{viewState.selectedLog?.accomplishment}</TableCell>
+                                                </TableRow>
+                                            )}
                                             <TableRow>
                                                 <TableCell component="th" scope="row">
                                                     {getString('Activity_tracking_column_comment')}
