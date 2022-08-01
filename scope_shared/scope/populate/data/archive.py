2abdb03d8053a8c8407dd588a5eddb669c16d4f7
James Fogarty <jayfo@users.noreply.github.com>
2022-08-01 16:19:48 -0700

    Initial successful export and restore

--- scope_shared/scope/populate/data/archive.py
+++ scope_shared/scope/populate/data/archive.py
@@ -3,7 +3,7 @@ import copy
 import json
 from pathlib import Path
 import pyzipper
-from typing import Dict, List, Optional
+from typing import Dict, Optional, Tuple, Union
 
 
 class Archive(contextlib.AbstractContextManager):
@@ -44,8 +44,63 @@ class Archive(contextlib.AbstractContextManager):
         # Close the archive
         self._zipfile.close()
 
+    @staticmethod
+    def collapse_revisions(
+        *,
+        documents: Dict[Path, dict]
+    ) -> Dict[Path, dict]:
+        """
+        Remove all documents that have been replaced by a more recent revision.
+        """
+        grouped_documents = Archive.group_revisions(documents=documents)
+
+        # Each group contains all versions of a specific document, keep only the most recent version
+        collapsed_documents: Dict[Path, dict] = {}
+        for (group_key_current, group_current) in grouped_documents.items():
+            key_most_recent = None
+            document_most_recent = None
+            for (key_current, document_current) in group_current.items():
+                if document_most_recent is None:
+                    key_most_recent = key_current
+                    document_most_recent = document_current
+                else:
+                    if document_current["_rev"] > document_most_recent["_rev"]:
+                        key_most_recent = key_current
+                        document_most_recent = document_current
+
+            collapsed_documents[key_most_recent] = document_most_recent
+
+        return collapsed_documents
+
+    @staticmethod
+    def group_revisions(
+        *,
+        documents: Dict[Path, dict]
+    ) -> Dict[Union[Tuple[str], Tuple[str, str]], Dict[Path, dict]]:
+        """
+        Group documents according to a tuple created from
+        - document "_type"
+        - any document "_set_id"
+        """
+        grouped_documents: Dict[Union[Tuple[str], Tuple[str, str]], Dict[Path, dict]] = {}
+        for (key_current, document_current) in documents.items():
+            if "_set_id" in document_current:
+                group_key_current = (document_current["_type"], document_current["_set_id"])
+            else:
+                group_key_current = (document_current["_type"])
+
+            if group_key_current not in grouped_documents:
+                grouped_documents[group_key_current] = {}
+            grouped_documents[group_key_current][key_current] = document_current
+
+        return grouped_documents
+
     @property
     def documents(self) -> Dict[Path, dict]:
+        """
+        All documents in the archive.
+        """
+
         if self._documents is None:
             self._documents = {}
 
@@ -58,3 +113,69 @@ class Archive(contextlib.AbstractContextManager):
                 self._documents[Path(info_current.filename)] = document_current
 
         return copy.deepcopy(self._documents)
+
+    def documents_in_collection(
+        self,
+        *,
+        collection: str,
+        ignore_sentinel: bool,
+        collapsed: bool,
+    ) -> Dict[Path, dict]:
+        """
+        All documents in a specific collection.
+        """
+
+        # Obtain all documents in the archive
+        documents = self.documents
+
+        # Filter to those in this collection
+        documents_in_collection = {}
+        for (key_current, document_current) in documents.items():
+            parents_current = [
+                str(parent_current)
+                for parent_current
+                in key_current.parents
+                if str(parent_current) not in ["."]
+            ]
+
+            if collection in parents_current:
+                documents_in_collection[key_current] = document_current
+
+        # If there is a sentinel to ignore, do that
+        if ignore_sentinel:
+            documents_in_collection = {
+                key_current: document_current
+                for (key_current, document_current)
+                in documents_in_collection.items()
+                if document_current["_type"] != "sentinel"
+            }
+
+        # If documents are to be collapsed, do that
+        if collapsed:
+            return self.collapse_revisions(documents=documents_in_collection)
+        else:
+            return documents_in_collection
+
+    def patients_documents(
+        self,
+        *,
+        ignore_sentinel: bool,
+        collapsed: bool,
+    ) -> Dict[Path, dict]:
+        return self.documents_in_collection(
+            collection="patients",
+            ignore_sentinel=ignore_sentinel,
+            collapsed=collapsed,
+        )
+
+    def providers_documents(
+        self,
+        *,
+        ignore_sentinel: bool,
+        collapsed: bool,
+    ) -> Dict[Path, dict]:
+        return self.documents_in_collection(
+            collection="providers",
+            ignore_sentinel=ignore_sentinel,
+            collapsed=collapsed,
+        )
---
+++ scope_shared/scope/populate/data/rule_archive_restore.py
@@ -0,0 +1,211 @@
+import json
+from pathlib import Path
+import pymongo.database
+from typing import List, Optional
+
+import scope.database.patients
+import scope.populate.data.archive
+from scope.populate.types import PopulateAction, PopulateContext, PopulateRule
+import scope.schema
+import scope.schema_utils
+
+ACTION_NAME = "archive_restore"
+
+
+class ArchiveRestore(PopulateRule):
+    def match(
+        self,
+        *,
+        populate_context: PopulateContext,
+        populate_config: dict,
+    ) -> Optional[PopulateAction]:
+        for action_current in populate_config["actions"]:
+            if action_current["action"] != ACTION_NAME:
+                continue
+
+            return _ArchiveRestoreAction(
+                archive=action_current["archive"],
+            )
+
+        return None
+
+
+class _ArchiveRestoreAction(PopulateAction):
+    archive: str
+
+    def __init__(
+        self,
+        *,
+        archive: str,
+    ):
+        self.archive = archive
+
+    def prompt(self) -> List[str]:
+        return ["Restore archive: {}".format(self.archive)]
+
+    def perform(
+        self,
+        *,
+        populate_context: PopulateContext,
+        populate_config: dict,
+    ) -> dict:
+        # Retrieve and remove our action
+        action = None
+        for action_current in populate_config["actions"]:
+            if action_current["action"] != ACTION_NAME:
+                continue
+            if action_current["archive"] != self.archive:
+                continue
+
+            action = action_current
+            break
+
+        # Confirm we found the action
+        if not action:
+            raise ValueError("populate_config was modified")
+
+        # Remove the action from the pending list
+        populate_config["actions"].remove(action)
+
+        # Ensure archive exists
+        if not Path(self.archive).exists():
+            raise ValueError("Archive does not exist")
+
+        # Prompt for a password
+        password = input("Enter archive password: ")
+
+        # Perform the restore
+        _archive_restore(
+            database=populate_context.database,
+            archive_path=Path(self.archive),
+            password=password,
+        )
+
+        return populate_config
+
+
+def _archive_restore(
+    *,
+    database: pymongo.database.Database,
+    archive_path: Path,
+    password: str,
+):
+    with scope.populate.data.archive.Archive(
+        archive_path=archive_path,
+        password=password,
+    ) as archive:
+        # Verify every document matches the document schema
+        for document_current in archive.documents.values():
+            # Assert the document schema
+            scope.schema_utils.assert_schema(
+                data=document_current,
+                schema=scope.schema.document_schema,
+            )
+
+        # Providers are simple documents in "providers" collection.
+        # Database initialization will ensure "providers" collection exists.
+        # But it will have an existing "sentinel" that needs to be deleted.
+        # Simply restore the "providers" collection.
+        restore_providers_documents = archive.providers_documents(
+            ignore_sentinel=False,
+            collapsed=False,
+        ).values()
+        providers_collection = database["providers"]
+        result = providers_collection.delete_one(
+            filter={
+                "_type": "sentinel"
+            }
+        )
+        if result.deleted_count != 1:
+            raise RuntimeError("Failed to delete existing sentinel in providers collection")
+        result = providers_collection.insert_many(
+            documents=restore_providers_documents,
+            ordered=False,
+        )
+        if not result.acknowledged:
+            raise RuntimeError("Failed to restore providers collection")
+
+        # Patients each have a document in "patients" and then a dedicated document.
+        # Database initialization will ensure "patients" collection exists.
+        # But it will have an existing "sentinel" that needs to be deleted.
+        # First restore the "patients" collection.
+        # Then each patient collection needs to be created.
+        # Restore patients by ensuring each step in patient creation.
+
+        # Restore the "patients" collection
+        restore_patients_documents = archive.patients_documents(
+            ignore_sentinel=False,
+            collapsed=False,
+        ).values()
+        patients_collection = database["patients"]
+        result = patients_collection.delete_one(
+            filter={
+                "_type": "sentinel"
+            }
+        )
+        if result.deleted_count != 1:
+            raise RuntimeError("Failed to delete existing sentinel in patients collection")
+        result = patients_collection.insert_many(
+            documents=restore_patients_documents,
+            ordered=False,
+        )
+        if not result.acknowledged:
+            raise RuntimeError("Failed to restore patients collection")
+
+        # Iterate over each patient, restore its collection and documents
+        for restore_patient_document_current in archive.patients_documents(
+            ignore_sentinel=True,
+            collapsed=True,
+        ).values():
+            # Recover fields we need from the patient document
+            patient_id = restore_patient_document_current["patientId"]
+            patient_collection_name = restore_patient_document_current["collection"]
+            patient_name = restore_patient_document_current["name"]
+            patient_mrn = restore_patient_document_current["MRN"]
+
+            # Ensure the patient collection
+            patient_collection = scope.database.patients.ensure_patient_collection(
+                database=database,
+                patient_id=patient_id,
+            )
+            if patient_collection.name != patient_collection_name:
+                raise RuntimeError("Patient collection name changed")
+
+            # Restore patient documents, including the sentinel
+            restore_patient_documents = archive.documents_in_collection(
+                collection=patient_collection_name,
+                ignore_sentinel=False,
+                collapsed=False,
+            ).values()
+            result = patient_collection.delete_one(
+                filter={
+                    "_type": "sentinel"
+                }
+            )
+            if result.deleted_count != 1:
+                raise RuntimeError("Failed to delete existing sentinel in patient collection")
+            result = patient_collection.insert_many(
+                documents=restore_patient_documents
+            )
+            if not result.acknowledged:
+                raise RuntimeError("Failed to restore patient collection: {}".format(patient_collection_name))
+
+            # Ensure minimal documents.
+            # This will usually do nothing, as the existing documents were already restored.
+            scope.database.patients.ensure_patient_documents(
+                database=database,
+                patient_collection=patient_collection,
+                patient_id=patient_id,
+                patient_name=patient_name,
+                patient_mrn=patient_mrn
+            )
+
+            # Ensure a patient identity documents.
+            # This will usually do nothing, as the existing documents were already restored.
+            scope.database.patients.ensure_patient_identity(
+                database=database,
+                patient_collection=patient_collection,
+                patient_id=patient_id,
+                patient_name=patient_name,
+                patient_mrn=patient_mrn
+            )
