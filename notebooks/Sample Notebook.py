084045137fe701c55624ea9634ccc296e0e69c10
Anant Mittal <anmittal@cs.washington.edu>
2022-09-27 13:11:54 -0700

    Added Sample Notebook.py

---
+++ notebooks/Sample Notebook.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# Extract the `archive_{}_{release}_{date}.zip` folder in data folder.
+
+import pandas as pd
+import pathlib
+
+paths = Path("./data/patients").glob("*.json")
+patients_df = pd.DataFrame([pd.read_json(p, typ="series") for p in paths])
+
+patients_df.head()
+
+# Number of patients
+patients_df["patientId"].nunique()
+
+
