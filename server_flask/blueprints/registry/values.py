19283c8222dd6f4f125c0ecfc70929697282a8b8
Anant Mittal <anmittal@cs.washington.edu>
2022-10-29 14:43:24 -0700

    Intitiate delete code in flask layer for value

--- server_flask/blueprints/registry/values.py
+++ server_flask/blueprints/registry/values.py
@@ -157,3 +157,51 @@ def put_value(patient_id, value_id):
         return {
             "value": document_response,
         }
+
+
+@values_blueprint.route(
+    "/<string:patient_id>/value/<string:value_id>",
+    methods=["DELETE"],
+)
+@flask_json.as_json
+def delete_value(patient_id, value_id):
+    context = request_context.authorized_for_patient(patient_id=patient_id)
+    patient_collection = context.patient_collection(patient_id=patient_id)
+
+    if_match_header = flask.request.headers.get("If-Match")
+    if if_match_header is None:
+        request_utils.abort_delete_without_if_match_header()
+
+    # Delete the document
+    try:
+        result = scope.database.patient.values.delete_value(
+            collection=patient_collection, set_id=value_id, rev=int(if_match_header)
+        )
+    except pymongo.errors.DuplicateKeyError:
+        # NOTE: Possible reasons to arrive here:
+        # (1): document with (_rev == if_match_header + 1) already exists in which case current "value" document will be returned with 409.
+        # (2): deleted document with (_rev == if_match_header + 1) already exists in which case null "value" document will be returned with 409.
+
+        # Indicates a revision race condition, return error with current revision
+        document_conflict = scope.database.patient.values.get_value(
+            collection=patient_collection, set_id=value_id
+        )
+        # Validate and normalize the response
+        document_conflict = request_utils.singleton_put_response_validate(
+            document=document_conflict
+        )
+
+        request_utils.abort_revision_conflict(
+            document={
+                "value": document_conflict,
+            }
+        )
+    else:
+        # Validate and normalize the response
+        document_response = request_utils.singleton_put_response_validate(
+            document=result.document,
+        )
+
+        return {
+            "value": document_response,
+        }
