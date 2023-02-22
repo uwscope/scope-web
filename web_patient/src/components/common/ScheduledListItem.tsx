2ea6ad032d15ebf69eb2ad97f7bdd181123642cd
Anant Mittal <anmittal@cs.washington.edu>
2023-02-22 09:01:16 -0800

    ScheduledListItem.tsx: activity is required in scheduled activity data snapshot

--- web_patient/src/components/common/ScheduledListItem.tsx
+++ web_patient/src/components/common/ScheduledListItem.tsx
@@ -19,7 +19,7 @@ export const ScheduledListItem: FunctionComponent<IScheduledListItemProps> = (pr
                 {item.completed ? <CheckIcon /> : <RadioButtonUncheckedIcon />}
             </ListItemIcon>
             <ListItemText
-                primary={<Typography noWrap>{item.dataSnapshot?.activity?.name}</Typography>}
+                primary={<Typography noWrap>{item.dataSnapshot.activity.name}</Typography>}
                 secondary={getTaskItemDueTimeString(item, new Date())}
             />
         </ListItem>
