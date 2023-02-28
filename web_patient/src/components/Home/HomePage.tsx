873fc6532b744a81c35b5480c0bdb2564da5dbfa
Anant Mittal <anmittal@cs.washington.edu>
2023-02-27 17:51:03 -0800

    HomePage.tsx: Add section for overdue items

--- web_patient/src/components/Home/HomePage.tsx
+++ web_patient/src/components/Home/HomePage.tsx
@@ -1,7 +1,7 @@
-import { Avatar, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
+import { Avatar, Divider, Grid, List, ListItem, ListItemAvatar, ListItemText, Switch, Typography } from '@mui/material';
 import withTheme from '@mui/styles/withTheme';
 import { action } from 'mobx';
-import { observer } from 'mobx-react';
+import { observer, useLocalObservable } from 'mobx-react';
 import React, { Fragment, FunctionComponent } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { IScheduledActivity } from 'shared/types';
@@ -28,6 +28,18 @@ const CompactList = withTheme(
 export const HomePage: FunctionComponent = observer(() => {
     const rootStore = useStores();
     const navigate = useNavigate();
+
+    const viewState = useLocalObservable<{
+        showAllOverdueActivities: boolean;
+    }>(() => ({
+        showAllOverdueActivities: false,
+    }));
+
+    const handleOverdueViewToggle = action((event: React.ChangeEvent<HTMLInputElement>) => {
+        viewState.showAllOverdueActivities = event.target.checked;
+    });
+
+    const overdueItems = rootStore.patientStore.getOverdueItems(1);
 
     const onTaskClick = action((item: IScheduledActivity) => () => {
         navigate(
@@ -146,6 +158,58 @@ export const HomePage: FunctionComponent = observer(() => {
                     <Typography variant="body2">{getString('Home_plan_done')}</Typography>
                 )}
             </Section>
+            {!!overdueItems && overdueItems.length > 0 && (
+                <Section title={getString('Home_overdue_title')}>
+                    <Grid container alignItems="center" spacing={1} justifyContent="center">
+                        <Grid item>
+                            <Typography
+                                variant="subtitle2"
+                                color={viewState.showAllOverdueActivities ? 'textSecondary' : 'textPrimary'}>
+                                {getString('Home_overdue_pending')}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Switch
+                                checked={viewState.showAllOverdueActivities}
+                                color="default"
+                                onChange={handleOverdueViewToggle}
+                                name="onOff"
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Typography
+                                variant="subtitle2"
+                                color={viewState.showAllOverdueActivities ? 'textPrimary' : 'textSecondary'}>
+                                {getString('Home_overdue_all')}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+
+                    {viewState.showAllOverdueActivities ? (
+                        <CompactList>
+                            {overdueItems.map((item, idx) => (
+                                <Fragment key={`${item.scheduledActivityId}-${idx}`}>
+                                    <ScheduledListItem item={item} onClick={onTaskClick(item)} />
+                                    {idx < overdueItems.length - 1 && <Divider variant="middle" />}
+                                </Fragment>
+                            ))}
+                        </CompactList>
+                    ) : (
+                        <CompactList>
+                            {overdueItems
+                                .filter((i) => !i.completed)
+                                .map((item, idx) => (
+                                    <Fragment key={`${item.scheduledActivityId}-${idx}`}>
+                                        <ScheduledListItem item={item} onClick={onTaskClick(item)} />
+                                        {idx < overdueItems.filter((i) => !i.completed).length - 1 && (
+                                            <Divider variant="middle" />
+                                        )}
+                                    </Fragment>
+                                ))}
+                        </CompactList>
+                    )}
+                </Section>
+            )}
         </MainPage>
     );
 });
