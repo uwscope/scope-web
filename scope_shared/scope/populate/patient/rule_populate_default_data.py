80547cd2bdc4f028a9ab285e162266ddece1ec48
Anant Mittal <anmittal@cs.washington.edu>
2024-08-21 12:44:42 -0700

    Populate new patient with empty review mark

--- scope_shared/scope/populate/patient/rule_populate_default_data.py
+++ scope_shared/scope/populate/patient/rule_populate_default_data.py
@@ -1,8 +1,15 @@
+import datetime
+import faker as _faker
 import pymongo.database
+import pytz
 from typing import List, Optional
 
+import scope.database.date_utils as date_utils
+import scope.database.patient.review_marks
 import scope.database.patients
 from scope.populate.types import PopulateAction, PopulateContext, PopulateRule
+import scope.testing.fake_data.fixtures_fake_review_mark
+
 
 ACTION_NAME = "populate_default_data"
 
@@ -70,6 +77,7 @@ class _PopulateDefaultDataAction(PopulateAction):
         # Perform the populate
         _populate_default_data(
             database=populate_context.database,
+            faker=populate_context.faker,
             patient_config=patient_config,
         )
 
@@ -79,6 +87,7 @@ class _PopulateDefaultDataAction(PopulateAction):
 def _populate_default_data(
     *,
     database: pymongo.database.Database,
+    faker: _faker.Faker,
     patient_config: dict,
 ) -> None:
     """
@@ -101,3 +110,31 @@ def _populate_default_data(
 
     # Default population is currently None.
     # Rule left in place for future use.
+
+    def _review_mark():
+        # Obtain a fake review mark
+        fake_review_mark_factory = (
+            scope.testing.fake_data.fixtures_fake_review_mark.fake_review_mark_factory(
+                faker_factory=faker
+            )
+        )
+        fake_review_mark = fake_review_mark_factory()
+        if "effectiveDateTime" in fake_review_mark:
+            del fake_review_mark["effectiveDateTime"]
+        if "providerId" in fake_review_mark:
+            del fake_review_mark["providerId"]
+
+        fake_review_mark.update(
+            {
+                "editedDateTime": date_utils.format_datetime(
+                    pytz.utc.localize(datetime.datetime.utcnow())
+                ),
+            }
+        )
+
+        scope.database.patient.review_marks.post_review_mark(
+            collection=patient_collection,
+            review_mark=fake_review_mark,
+        )
+
+    _review_mark()
