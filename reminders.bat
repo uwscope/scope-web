bf21cbb3f0ec819f862d6c9d975ac2f25c293f3a
James Fogarty <jayfo@users.noreply.github.com>
2025-02-07 10:27:50 -0800

    Delete Reminders Batch from Repository

--- reminders.bat
+++
@@ -1,9 +0,0 @@
-C:\Python39\Scripts\pipenv.exe run invoke reminders.dev.email -t jfogarty@cs.washington.edu
-
-C:\Python39\Scripts\pipenv.exe run invoke reminders.demo.email -t jfogarty@cs.washington.edu
-
-C:\Python39\Scripts\pipenv.exe run invoke reminders.multicare.email -t jfogarty@cs.washington.edu
-
-C:\Python39\Scripts\pipenv.exe run invoke reminders.scca.email -t jfogarty@cs.washington.edu
-
-EXIT /B 0
